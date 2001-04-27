97d67cd7e60c8bcbbebd4ef3b3a6dfc981bd9771
Sebastian Trueg <sebastian@trueg.de>
2001-04-27 14:04:23 +0000

    added file to cvs
    svn path=/trunk/kdemultimedia/k3b/; revision=94178

---
+++ src/cdinfo/k3bcdinfo.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+                          k3bcdinfo.h  -  description
+                             -------------------
+    begin                : Sun Apr 22 2001
+    copyright            : (C) 2001 by Sebastian Trueg
+    email                : trueg@informatik.uni-freiburg.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef K3BCDINFO_H
+#define K3BCDINFO_H
+
+
+/**
+  *@author Sebastian Trueg
+  */
+
+class K3bCDInfo {
+public: 
+	K3bCDInfo();
+	~K3bCDInfo();
+};
+
+#endif
