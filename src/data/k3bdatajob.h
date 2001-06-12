7a02a03a0a1ed3a934c894dc3a90920258e0e86c
Sebastian Trueg <sebastian@trueg.de>
2001-06-12 20:48:30 +0000

    added to cvs
    svn path=/trunk/kdemultimedia/k3b/; revision=101870

---
+++ src/data/k3bdatajob.h
@@ -0,0 +1,66 @@
+/***************************************************************************
+                          k3bdatajob.h  -  description
+                             -------------------
+    begin                : Tue May 15 2001
+    copyright            : (C) 2001 by Sebastian Trueg
+    email                : trueg@informatik.uni-freiburg.de
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef K3BDATAJOB_H
+#define K3BDATAJOB_H
+
+#include "../k3bjob.h"
+
+#include <kprocess.h>
+
+class K3bDataDoc;
+class QString;
+
+
+/**
+  *@author Sebastian Trueg
+  */
+
+class K3bDataJob : public K3bBurnJob
+{
+	Q_OBJECT
+	
+public:
+	K3bDataJob( K3bDataDoc* );
+	~K3bDataJob();
+	
+	K3bDoc* doc() const;
+		
+public slots:
+	void cancel();
+	void start();
+
+protected slots:
+	void slotParseCdrecordOutput( KProcess*, char*, int );
+	void slotParseMkisofsOutput( KProcess*, char*, int );
+	void slotMkisofsFinished();
+	void slotCdrecordFinished();
+	void slotParseMkisofsSize(KProcess*, char*, int);
+		
+private:
+	K3bDataDoc* m_doc;
+	QString m_pathSpecFile;
+	KProcess* m_process;
+
+	QString m_isoSize;
+		
+	void writeImage();
+	void writeCD();
+	void addMkisofsParameters();
+};
+
+#endif
