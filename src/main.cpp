4eff95bf2617025b0f6a72d9762348a19dadbf69
Sebastian Trueg <sebastian@trueg.de>
2001-10-17 09:01:25 +0000

    fixed Thomas' e-mail adress
    svn path=/trunk/kdemultimedia/k3b/; revision=118269

--- src/main.cpp
+++ src/main.cpp
@@ -40,7 +40,7 @@ int main(int argc, char *argv[])
 			VERSION, description, KAboutData::License_GPL,
 			"(c) 2001, Sebastian Trueg", 0, 0, "trueg@informatik.uni-freiburg.de");
   aboutData.addAuthor("Sebastian Trueg",I18N_NOOP("Maintainer"), "trueg@informatik.uni-freiburg.de");
-  aboutData.addAuthor("Thomas Froescher",I18N_NOOP("Developer"), "tfroescher@gmx.net");
+  aboutData.addAuthor("Thomas Froescher",I18N_NOOP("Developer"), "tfroescher@gmx.de");
 	
   KCmdLineArgs::init( argc, argv, &aboutData );
   KCmdLineArgs::addCmdLineOptions( options ); // Add our own options.
