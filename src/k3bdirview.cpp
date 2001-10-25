5fb6e1244ea8875ed53be6ce15118f5183285583
Sebastian Trueg <sebastian@trueg.de>
2001-10-25 09:16:24 +0000

    removed text on the icons
    svn path=/trunk/kdemultimedia/k3b/; revision=119348

--- src/k3bdirview.cpp
+++ src/k3bdirview.cpp
@@ -4,7 +4,7 @@
     begin                : Mon Mar 26 2001
     copyright            : (C) 2001 by Sebastian Trueg
     email                : trueg@informatik.uni-freiburg.de
- ***************************************************************************/
+***************************************************************************/
 
 /***************************************************************************
  *                                                                         *
@@ -52,27 +52,27 @@
 /////////////////////////////////////////////////////////////////////
 
 K3bDirView::PrivateFileView::PrivateFileView( QWidget* parent, const char* name )
- : KFileDetailView( parent, name )
+  : KFileDetailView( parent, name )
 {
-	setDragEnabled( true );
-	KFileDetailView::setSelectionMode( KFile::Extended );
-	KListView::setSelectionModeExt( KListView::Extended );
+  setDragEnabled( true );
+  KFileDetailView::setSelectionMode( KFile::Extended );
+  KListView::setSelectionModeExt( KListView::Extended );
 }
 
 	
 QDragObject* K3bDirView::PrivateFileView::dragObject() const
 {
-	if( !currentItem() )
-		return 0;
+  if( !currentItem() )
+    return 0;
 	
-	const KFileViewItemList* list = KFileView::selectedItems();
-	QListIterator<KFileViewItem> it(*list);
-	QStrList dragstr;
+  const KFileViewItemList* list = KFileView::selectedItems();
+  QListIterator<KFileViewItem> it(*list);
+  QStrList dragstr;
 	
-	for( ; it.current(); ++it )
-		dragstr.append( it.current()->url().path(-1) );
+  for( ; it.current(); ++it )
+    dragstr.append( it.current()->url().path(-1) );
 		
-	return new QUriDrag( dragstr, viewport() );
+  return new QUriDrag( dragstr, viewport() );
 }
 
 
@@ -80,55 +80,55 @@ QDragObject* K3bDirView::PrivateFileView::dragObject() const
 ////////////////////////////////////////////////////////////////////
 
 K3bDirView::K3bDirView(QWidget *parent, const char *name )
- : QVBox(parent, name)
+  : QVBox(parent, name)
 {
-	m_mainSplitter = new QSplitter( this );
-	QVBox* box = new QVBox( m_mainSplitter );
-	QWidget* box2 = new QWidget( m_mainSplitter );
-	QGridLayout* _box2Layout = new QGridLayout( box2 );
-	_box2Layout->setSpacing( 0 );
+  m_mainSplitter = new QSplitter( this );
+  QVBox* box = new QVBox( m_mainSplitter );
+  QWidget* box2 = new QWidget( m_mainSplitter );
+  QGridLayout* _box2Layout = new QGridLayout( box2 );
+  _box2Layout->setSpacing( 0 );
 	
-	m_kiotree = new KioTree( box );
-   m_kiotree->addTopLevelDir( KURL( QDir::homeDirPath() ), "Home" );
-   m_kiotree->addTopLevelDir( KURL( "/" ), "Root" );
+  m_kiotree = new KioTree( box );
+  m_kiotree->addTopLevelDir( KURL( QDir::homeDirPath() ), "Home" );
+  m_kiotree->addTopLevelDir( KURL( "/" ), "Root" );
 
-	m_fileView = new KDirOperator( QDir::home().absPath(), box2 );
-	m_fileView->setView( new PrivateFileView( m_fileView, "fileview" ) );	
+  m_fileView = new KDirOperator( QDir::home().absPath(), box2 );
+  m_fileView->setView( new PrivateFileView( m_fileView, "fileview" ) );	
 	
-	// add some actions to the toolbar
-	KAction* _actionUp = m_fileView->actionCollection()->action("up");
-	KAction* _actionHome = m_fileView->actionCollection()->action("home");
-	KAction* _actionReload = m_fileView->actionCollection()->action("reload");
+  // add some actions to the toolbar
+  KAction* _actionUp = m_fileView->actionCollection()->action("up");
+  KAction* _actionHome = m_fileView->actionCollection()->action("home");
+  KAction* _actionReload = m_fileView->actionCollection()->action("reload");
 	
-	QPushButton* _buttonUp = new QPushButton( _actionUp->iconSet(), _actionUp->text(), box2 );
-	QPushButton* _buttonHome = new QPushButton( _actionHome->iconSet(), _actionHome->text(), box2 );
-	QPushButton* _buttonReload = new QPushButton( _actionReload->iconSet(), _actionReload->text(), box2 );
+  QPushButton* _buttonUp = new QPushButton( _actionUp->iconSet(), "", box2 );
+  QPushButton* _buttonHome = new QPushButton( _actionHome->iconSet(), "", box2 );
+  QPushButton* _buttonReload = new QPushButton( _actionReload->iconSet(), "", box2 );
 	
-	// add the buttons and the fileview in the layout
-//	QGridLayout* _buttonLayout = new QGridLayout;
-//	_buttonLayout->setMargin( 5 );
-//	_buttonLayout->setSpacing( 0 );
-//	_buttonLayout->addWidget( _buttonUp, 0, 0 );
-//	_buttonLayout->addWidget( _buttonHome, 0, 1 );
-//	_buttonLayout->addWidget( _buttonReload, 0, 2 );
-	_box2Layout->addWidget( _buttonUp, 0, 0 );
-	_box2Layout->addWidget( _buttonHome, 0, 1 );
-	_box2Layout->addWidget( _buttonReload, 0, 2 );
-//	_box2Layout->addLayout( _buttonLayout, 0, 0 );
-	_box2Layout->addMultiCellWidget( m_fileView, 2, 2, 0, 3 );
-	_box2Layout->setRowStretch( 2, 1 );
+  // add the buttons and the fileview in the layout
+  //	QGridLayout* _buttonLayout = new QGridLayout;
+  //	_buttonLayout->setMargin( 5 );
+  //	_buttonLayout->setSpacing( 0 );
+  //	_buttonLayout->addWidget( _buttonUp, 0, 0 );
+  //	_buttonLayout->addWidget( _buttonHome, 0, 1 );
+  //	_buttonLayout->addWidget( _buttonReload, 0, 2 );
+  _box2Layout->addWidget( _buttonUp, 0, 0 );
+  _box2Layout->addWidget( _buttonHome, 0, 1 );
+  _box2Layout->addWidget( _buttonReload, 0, 2 );
+  //	_box2Layout->addLayout( _buttonLayout, 0, 0 );
+  _box2Layout->addMultiCellWidget( m_fileView, 2, 2, 0, 3 );
+  _box2Layout->setRowStretch( 2, 1 );
 	
-	// the buttons should be square
-	_buttonUp->setMaximumWidth( _buttonUp->height() );
-	_buttonHome->setMaximumWidth( _buttonUp->height() );
-	_buttonReload->setMaximumWidth( _buttonUp->height() );
+  // the buttons should be square
+  _buttonUp->setMaximumWidth( _buttonUp->height() );
+  _buttonHome->setMaximumWidth( _buttonUp->height() );
+  _buttonReload->setMaximumWidth( _buttonUp->height() );
 	
-	// connect to the actions
-	connect( _buttonUp, SIGNAL(clicked()), _actionUp, SLOT(activate()) );
-	connect( _buttonHome, SIGNAL(clicked()), _actionHome, SLOT(activate()) );
-	connect( _buttonReload, SIGNAL(clicked()), _actionReload, SLOT(activate()) );
+  // connect to the actions
+  connect( _buttonUp, SIGNAL(clicked()), _actionUp, SLOT(activate()) );
+  connect( _buttonHome, SIGNAL(clicked()), _actionHome, SLOT(activate()) );
+  connect( _buttonReload, SIGNAL(clicked()), _actionReload, SLOT(activate()) );
 
-	connect( m_kiotree, SIGNAL(urlActivated(const KURL&)), this, SLOT(slotDirActivated(const KURL&)) );	
+  connect( m_kiotree, SIGNAL(urlActivated(const KURL&)), this, SLOT(slotDirActivated(const KURL&)) );	
 }
 
 
@@ -138,13 +138,13 @@ K3bDirView::~K3bDirView(){
 
 void K3bDirView::slotViewChanged( KFileView* newView )
 {
-	newView->setSelectionMode( KFile::Extended );
-	if( KListView* _x = dynamic_cast<KListView*>( newView->widget() ) )
-		_x->setDragEnabled( true );
+  newView->setSelectionMode( KFile::Extended );
+  if( KListView* _x = dynamic_cast<KListView*>( newView->widget() ) )
+    _x->setDragEnabled( true );
 }
 
 
 void K3bDirView::slotDirActivated( const KURL& url )
 {
-	m_fileView->setURL( url, true );
+  m_fileView->setURL( url, true );
 }
