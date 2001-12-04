999efe38d1915c7488d7bf2a9e63c5c5f5f0df84
Sebastian Trueg <sebastian@trueg.de>
2001-12-04 20:21:03 +0000

    changed info to version 0.5.0
    svn path=/trunk/kdemultimedia/k3b/; revision=125543

--- src/data/k3bisoimagewritingdialog.cpp
+++ src/data/k3bisoimagewritingdialog.cpp
@@ -203,6 +203,11 @@ void K3bIsoImageWritingDialog::slotFindImageFile()
 
 void K3bIsoImageWritingDialog::slotWriterChanged()
 {
-  if( !m_writerSelectionWidget->writerDevice()->burnproof() )
+  if( !m_writerSelectionWidget->writerDevice()->burnproof() ) {
     m_checkBurnProof->setChecked( false );
+    m_checkBurnProof->setDisabled( true );
+  }
+  else {
+    m_checkBurnProof->setEnabled( true );
+  }
 }
