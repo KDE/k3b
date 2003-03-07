d14984ed7759fe1ba7d64402c2b0693ee4d6afce
Sebastian Trueg <sebastian@trueg.de>
2003-03-07 10:05:18 +0000

    added new gpl header
    svn path=/trunk/kdeextragear-1/k3b/; revision=212040

--- src/tools/k3blistview.cpp
+++ src/tools/k3blistview.cpp
@@ -1,22 +1,18 @@
-/***************************************************************************
-                              k3blistview.cpp
-                                   -
-A KlistView with feature to display text and pixmap if no item is in the view.
+/* 
+ *
+ * $Id: $
+ * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
 
-                             -------------------
-    begin                : Wed Sep  4 12:56:10 CEST 2002
-    copyright            : (C) 2002 by Sebastian Trueg
-    email                : trueg@informatik.uni-freiburg.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 
 
 #include "k3blistview.h"
