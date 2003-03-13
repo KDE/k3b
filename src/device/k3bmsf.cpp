b35c40dfbfa4961b9779c09f1deb2c7b28f86ba4
Sebastian Trueg <sebastian@trueg.de>
2003-03-13 16:08:37 +0000

    added MSF class
    svn path=/trunk/kdeextragear-1/k3b/; revision=213618

---
+++ src/device/k3bmsf.cpp
@@ -0,0 +1,160 @@
+/* 
+ *
+ * $Id$
+ * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "k3bmsf.h"
+
+K3b::Msf::Msf()
+  : m_minutes(0),
+    m_seconds(0),
+    m_frames(0)
+{
+}
+
+K3b::Msf::Msf( const K3b::Msf& m )
+  : m_minutes(m.minutes()),
+    m_seconds(m.seconds()),
+    m_frames(m.frames())
+{
+}
+
+K3b::Msf::Msf( int m, int s, int f )
+  : m_minutes(m),
+    m_seconds(s),
+    m_frames(f)
+{
+}
+
+K3b::Msf::Msf( int i )
+  : m_minutes(0),
+    m_seconds(0),
+    m_frames(i)
+{
+  makeValid();
+}
+
+K3b::Msf& K3b::Msf::operator=( const K3b::Msf& m )
+{
+  m_frames = m.frames();
+  m_seconds = m.seconds();
+  m_minutes = m.minutes();
+  return *this;
+}
+
+K3b::Msf& K3b::Msf::operator=( int i )
+{
+  m_frames = i;
+  m_seconds = 0;
+  m_minutes = 0;
+  makeValid();
+  return *this;
+}
+
+K3b::Msf& K3b::Msf::operator+=( const K3b::Msf& m )
+{
+  m_frames += m.frames();
+  m_seconds += m.seconds();
+  m_minutes += m.minutes();
+  makeValid();
+  return *this;
+}
+
+K3b::Msf& K3b::Msf::operator+=( int i )
+{
+  m_frames += i;
+  makeValid();
+  return *this;
+}
+
+K3b::Msf& K3b::Msf::operator-=( const K3b::Msf& m )
+{
+  m_frames -= m.frames();
+  m_seconds -= m.seconds();
+  m_minutes -= m.minutes();
+  makeValid();
+  return *this;
+}
+
+K3b::Msf& K3b::Msf::operator-=( int i )
+{
+  m_frames -= i;
+  makeValid();
+  return *this;
+}
+
+
+void K3b::Msf::makeValid()
+{
+  if( m_frames < 0 ) {
+    int newFrames = m_frames/-75 + 1;
+    m_seconds -= newFrames;
+    m_frames += 75*newFrames;
+  }
+  m_seconds += m_frames/75;
+  m_frames = m_frames % 75;
+  if( m_seconds < 0 ) {
+    int newSecs = m_seconds/-60 + 1;
+    m_minutes -= newSecs;
+    m_seconds += 60*newSecs;
+  }
+  m_minutes += m_seconds/60;
+  m_seconds = m_seconds % 60;
+  if( m_minutes < 0 ) {
+    m_minutes = 0;
+    m_seconds = 0;
+    m_frames = 0;
+  }
+}
+
+
+K3b::Msf K3b::operator+( const K3b::Msf& m1, const K3b::Msf& m2 )
+{
+  K3b::Msf msf(m1);
+  return msf += m2;
+}
+
+K3b::Msf K3b::operator+( const K3b::Msf& m, int i )
+{
+  K3b::Msf msf(m);
+  return msf += i;
+}
+
+K3b::Msf K3b::operator-( const K3b::Msf& m1, const K3b::Msf& m2 )
+{
+  K3b::Msf msf(m1);
+  return msf -= m2;
+}
+
+K3b::Msf K3b::operator-( const K3b::Msf& m, int i )
+{
+  K3b::Msf msf(m);
+  return msf -= i;
+}
+
+bool K3b::operator==( const K3b::Msf& m1, const K3b::Msf& m2 )
+{
+  return ( m1.minutes() == m2.minutes() &&
+	   m1.seconds() == m2.seconds() &&
+	   m1.frames() == m2.frames() );
+}
+
+bool K3b::operator<( const K3b::Msf& m1, const K3b::Msf& m2 )
+{
+  return ( m1.totalFrames() < m2.totalFrames() );
+}
+
+bool K3b::operator>( const K3b::Msf& m1, const K3b::Msf& m2 )
+{
+  return ( m1.totalFrames() > m2.totalFrames() );
+}
---
+++ src/device/k3bmsf.h
@@ -0,0 +1,69 @@
+/* 
+ *
+ * $Id$
+ * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+
+#ifndef _K3B_MSF_H_
+#define _K3B_MSF_H_
+
+namespace K3b
+{
+  /**
+   * int values are always treated as frames
+   * except in the set methods
+   */
+  class Msf
+    {
+    public:
+      Msf();
+      Msf( const Msf& );
+      Msf( int, int, int );
+      Msf( int );
+
+      Msf& operator=( const Msf& );
+      Msf& operator=( int );
+      Msf& operator+=( const Msf& );
+      Msf& operator+=( int );
+      Msf& operator-=( const Msf& );
+      Msf& operator-=( int );
+
+      int minutes() const { return m_minutes; }
+      int seconds() const { return m_seconds; }
+      int frames() const { return m_frames; }
+
+      int totalFrames() const { return ( m_minutes*60 + m_seconds )*75 + m_frames; }
+
+      void setMinutes( int m ) { m_minutes = m < 0 ? 0 : m; }
+      void setSeconds( int s ) { m_seconds = s < 0 ? 0 : s; }
+      void setFrames( int f ) { m_frames = f < 0 ? 0 : f; }
+
+    private:
+      void makeValid();
+      int m_minutes;
+      int m_seconds;
+      int m_frames;
+    };
+
+  Msf operator+( const Msf&, const Msf& );
+  Msf operator+( const Msf&, int );
+  Msf operator-( const Msf&, const Msf& );
+  Msf operator-( const Msf&, int );
+  bool operator==( const Msf&, const Msf& );
+  bool operator<( const Msf&, const Msf& );
+  bool operator>( const Msf&, const Msf& );
+};
+
+typedef K3b::Msf K3bMsf;
+
+#endif
