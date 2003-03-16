95d898f50ad5627e1d1896ad7ecd7d1167fc1b9b
Sebastian Trueg <sebastian@trueg.de>
2003-03-16 12:22:13 +0000

    added new gpl header
    svn path=/trunk/kdeextragear-1/k3b/; revision=214249

--- src/cdcopy/k3bcdcopyjob.h
+++ src/cdcopy/k3bcdcopyjob.h
@@ -1,19 +1,19 @@
-/***************************************************************************
-                          k3bcdcopyjob.h  -  description
-                             -------------------
-    begin                : Sun Mar 17 2002
-    copyright            : (C) 2002 by Sebastian Trueg
-    email                : trueg@informatik.uni-freiburg.de
- ***************************************************************************/
+/* 
+ *
+ * $Id: $
+ * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ *                    Klaus-Dieter Krannich <kd@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 
 #ifndef K3BCDCOPYJOB_H
 #define K3BCDCOPYJOB_H
