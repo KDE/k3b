25123318443417b5785501dbb87434e2a3694308
Thomas Froescher <tfroescher@gmx.de>
2003-05-18 15:08:28 +0000

    *** empty log message ***
    svn path=/trunk/kdeextragear-1/k3b/; revision=226556

--- src/videoEncoding/k3bdivxencodingprocess.cpp
+++ src/videoEncoding/k3bdivxencodingprocess.cpp
@@ -45,34 +45,42 @@ void K3bDivXEncodingProcess::start() {
   checkVobDirectory(); // checks if other files than .vob are in vob dir
 }
 void K3bDivXEncodingProcess::slotFinishedCheckVobDirectory() {
-  if ( m_data->getParaAc3().length() > 1 ) {
-    // use ac3
-    deleteIfos();
-  } else if ( m_data->isNormalize() ) {
-    infoMessage( i18n( "Copy IFO files to vob directory." ), INFO );
-    copyIfos(); // copy IFO files to enable dvd import mode of transcode
-  } else {
-    infoMessage( i18n( "Disable audio normalizing" ), INFO );
-    deleteIfos();
-  }
+    if( m_data->isTcDvdMode() || ( m_data->isNormalize() && !m_data->isAc3Set() ) ) {
+	    kdDebug() << "(K3bDivXEncodingProcess) Start transcode in dvd mode or mp3 audio gain mode." << endl;
+	    copyIfos(); // old: code directly from slotFinishedCopyIfos, before change to transcode -x dvd
+    } else {
+        deleteIfos();
+    }
 }
 
-void K3bDivXEncodingProcess::slotStartAudioProcessing( KIO::Job *job ) {
-  if ( job->error() > 0 ) {
-    kdDebug() << "(K3bDivXEncodingProcess) Job error during copy: " << job->errorString() << endl;
-  }
-  kdDebug() << "(K3bDivXEncodingProcess) Run transcode." << endl;
-  m_process = new KShellProcess;
-  const K3bExternalBin *tccatBin = k3bcore ->externalBinManager() ->binObject( "tccat" );
-  const K3bExternalBin *tcextractBin = k3bcore ->externalBinManager() ->binObject( "tcextract" );
-  const K3bExternalBin *tcdecodeBin = k3bcore ->externalBinManager() ->binObject( "tcdecode" );
-  const K3bExternalBin *tcscanBin = k3bcore ->externalBinManager() ->binObject( "tcscan" );
-  // parse audio for   gain to normalize
-  *m_process << "nice" << "-10";
-  *m_process << tccatBin->path << " -i" << m_data->getProjectDir() + "/vob" << "-t" << "vob" << "-P" << m_data->getTitle();
-  *m_process << "|" << tcextractBin->path << m_data->getParaAudioLanguage() << "-x" << "ac3" << "-t" << "vob";
-  *m_process << "|" << tcdecodeBin->path << "-x" << "ac3";
-  *m_process << "|" << tcscanBin->path << "-x" << "pcm";
+void K3bDivXEncodingProcess::slotFinishedCopyIfos(KIO::Job* job){
+    if ( job->error() > 0 ) {
+        kdDebug() << "(K3bDivXEncodingProcess) Job error during copy: " << job->errorString() << endl;
+    }
+    if ( m_data->getParaAc3().length() > 1 ) {
+        // use ac3
+        //deleteIfos(); // for using with transcode -x vob
+        slotStartEncoding(); 
+    } else if ( m_data->isNormalize() ) {
+        infoMessage( i18n( "Copy IFO files to vob directory." ), INFO );
+        slotStartAudioProcessing(); // -> deleteIfos -> slotStartEncoding
+        //copyIfos(); // for using with transcode -x vob
+    }
+}
+
+void K3bDivXEncodingProcess::slotStartAudioProcessing( ) {
+    kdDebug() << "(K3bDivXEncodingProcess) Run transcode." << endl;
+    m_process = new KShellProcess;
+    const K3bExternalBin *tccatBin = k3bMain() ->externalBinManager() ->binObject( "tccat" );
+    const K3bExternalBin *tcextractBin = k3bMain() ->externalBinManager() ->binObject( "tcextract" );
+    const K3bExternalBin *tcdecodeBin = k3bMain() ->externalBinManager() ->binObject( "tcdecode" );
+    const K3bExternalBin *tcscanBin = k3bMain() ->externalBinManager() ->binObject( "tcscan" );
+    // parse audio for   gain to normalize
+    *m_process << "nice" << "-10";
+    *m_process << tccatBin->path << " -i" << m_data->getProjectDir() + "/vob" << "-t" << "vob" << "-P" << m_data->getTitle();
+    *m_process << "|" << tcextractBin->path << m_data->getParaAudioLanguage() << "-x" << "ac3" << "-t" << "vob";
+    *m_process << "|" << tcdecodeBin->path << "-x" << "ac3";
+    *m_process << "|" << tcscanBin->path << "-x" << "pcm";
 
   kdDebug() << "(K3bDivXEncodingProcess)" + tccatBin->path + " -i " + m_data->getProjectDir() + "/vob -t vob -P " + m_data->getTitle()
     + " | " + tcextractBin->path + m_data->getParaAudioLanguage() + " -x ac3  -t vob "
@@ -96,98 +104,111 @@ void K3bDivXEncodingProcess::slotStartAudioProcessing( KIO::Job *job ) {
 }
 
 void K3bDivXEncodingProcess::slotStartEncoding() {
-  m_speedFlag = 301;
-  m_speedTrigger = 300;
-  m_speedInitialFlag = 0;
-  kdDebug() << "(K3bDivXEncodingProcess) Run transcode." << endl;
-  const K3bExternalBin *transcodeBin = k3bcore ->externalBinManager() ->binObject( "transcode" );
-  m_process = new KShellProcess;
+    m_speedFlag = 301;
+    m_speedTrigger = 300;
+    m_speedInitialFlag = 0;
+    QString debugPass( "" );
+    kdDebug() << "(K3bDivXEncodingProcess) Run transcode." << endl;
+    const K3bExternalBin *transcodeBin = k3bMain() ->externalBinManager() ->binObject( "transcode" );
+    m_process = new KShellProcess;
 
-  *m_process << "nice" << "-10";
-  *m_process << transcodeBin->path; //"/usr/local/bin/transcode -i ";
-  *m_process << " -i" << m_data->getProjectDir() + "/vob ";
-
-  *m_process << "-x" << "vob" << "-T" << m_data->getTitle() + ",-1,1" << m_data->getParaVideoBitrate() + "," + m_data->getKeyframes() + "," + m_data->getCrispness();
-  kdDebug() << "(K3bDivXEncodingProcess) Video: " + m_data->getParaVideoBitrate() + "," + m_data->getKeyframes() + "," + m_data->getCrispness() << endl;
-  *m_process << m_data->getParaYuv() << m_data->getParaAudioLanguage();
-  kdDebug() << "(K3bDivXEncodingProcess) Video: " + m_data->getParaYuv() + m_data->getParaAudioLanguage() << endl;
-  QString debugPass( "" );
-  if ( m_data->getParaAc3().length() > 1 ) {
-    *m_process << m_data->getParaAc3();
-    debugPass += m_data->getParaAc3();
-  } else {
-    *m_process << m_data->getParaAudioResample() << m_data->getParaAudioBitrate() << m_data->getParaAudioGain();
-    debugPass = debugPass + m_data->getParaAudioResample() + m_data->getParaAudioBitrate() + m_data->getParaAudioGain();
-  }
-  *m_process << m_data->getParaDeinterlace();
-  *m_process << " -o " << KShellProcess::quote( m_data->getAviFile() );
-  kdDebug() << "(K3bDivXEncodingProcess)  Out: " + m_data->getParaDeinterlace() + " -o " + m_data->getAviFile() << endl;
-
-  int top = m_data->getCropTop();
-  int left = m_data->getCropLeft();
-  int bottom = m_data->getCropBottom();
-  int right = m_data->getCropRight();
-  kdDebug() << "(K3bDivXEncodingProcess) Crop values t=" << top << ",l=" << left << ",b=" << bottom << ",r=" << right << endl;
-  kdDebug() << "(K3bDivXEncodingProcess) Resize factor height=" << m_data->getResizeHeight() << ", width=" << m_data->getResizeWidth() << endl;
-  *m_process << "-j" << QString::number( top ) + "," + QString::number( left ) + "," + QString::number( bottom ) + "," + QString::number( right );
-  *m_process << "-B" << QString::number( m_data->getResizeHeight() ) + "," + QString::number( m_data->getResizeWidth() ) + ",8";
-  if ( m_data->getCodecMode() == 2 ) {
-    if ( m_pass == 0 ) {
-      m_pass = 1;
-      *m_process << " -R 1," + m_data->getProjectDir() + "/tmp/divx4.log" << m_data->getParaCodec() << ",null";
-      debugPass += " -R 1," + m_data->getProjectDir() + "/tmp/divx4.log" + m_data->getParaCodec() + ",null";
+    *m_process << "nice" << "-10";
+    *m_process << transcodeBin->path; //"/usr/local/bin/transcode -i ";
+    *m_process << " -i" << m_data->getProjectDir() + "/vob ";
+    // -x vob
+    if( m_data->isTcDvdMode() ){
+  	*m_process << "-x" << "dvd";
+	debugPass +="-x dvd ";
+    } else {	
+	*m_process << "-x" << "vob";
+	debugPass +="-x vob ";
+    }	
+    *m_process << "-T" << m_data->getTitle() + ",-1,1" << m_data->getParaVideoBitrate() + "," + m_data->getKeyframes() + "," + m_data->getCrispness();
+    kdDebug() << "(K3bDivXEncodingProcess) Video: " + m_data->getParaVideoBitrate() + "," + m_data->getKeyframes() + "," + m_data->getCrispness() << endl;
+    *m_process << m_data->getParaYuv() << m_data->getParaAudioLanguage();
+    kdDebug() << "(K3bDivXEncodingProcess) Video: " + m_data->getParaYuv() + m_data->getParaAudioLanguage() << endl;
+    if ( m_data->getParaAc3().length() > 1 ) {
+        *m_process << m_data->getParaAc3();
+        debugPass += m_data->getParaAc3();
     } else {
-      m_pass = 2;
-      *m_process << " -R 2," + m_data->getProjectDir() + "/tmp/divx4.log" << m_data->getParaCodec();
-      debugPass += " -R 2," + m_data->getProjectDir() + "/tmp/divx4.log" + m_data->getParaCodec();
+        *m_process << m_data->getParaAudioResample() << m_data->getParaAudioBitrate() << m_data->getParaAudioGain();
+        debugPass = debugPass + m_data->getParaAudioResample() + m_data->getParaAudioBitrate() + m_data->getParaAudioGain();
     }
-  } else {
-    *m_process << m_data->getParaCodec();
-    debugPass += m_data->getParaCodec();
-  }
-  kdDebug() << "(K3bDivXEncodingProcess)" + transcodeBin->path + " -i " + m_data->getProjectDir() +
-    "/vob -x vob -T" + m_data->getTitle() + ",-1,1" +
+    *m_process << m_data->getParaDeinterlace();
+    kdDebug() << "(K3bDivXEncodingProcess)  Out: " + m_data->getParaDeinterlace() + " -o " + m_data->getAviFile() << endl;
+
+    int top = m_data->getCropTop();
+    int left = m_data->getCropLeft();
+    int bottom = m_data->getCropBottom();
+    int right = m_data->getCropRight();
+    kdDebug() << "(K3bDivXEncodingProcess) Crop values t=" << top << ",l=" << left << ",b=" << bottom << ",r=" << right << endl;
+    kdDebug() << "(K3bDivXEncodingProcess) Resize factor height=" << m_data->getResizeHeight() << ", width=" << m_data->getResizeWidth() << endl;
+    *m_process << "-j" << QString::number( top ) + "," + QString::number( left ) + "," + QString::number( bottom ) + "," + QString::number( right );
+    *m_process << "-B" << QString::number( m_data->getResizeHeight() ) + "," + QString::number( m_data->getResizeWidth() ) + ",8";
+    if ( m_data->getCodecMode() == 2 ) {
+        if ( m_pass == 0 ) {
+            m_pass = 1;
+            *m_process << " -o /dev/null";
+            *m_process << " -R 1," + m_data->getProjectDir() + "/tmp/divx4.log" << m_data->getParaCodec() << ",null";
+            debugPass += " -R 1," + m_data->getProjectDir() + "/tmp/divx4.log" + m_data->getParaCodec() + ",null -o /dev/null";
+        } else {
+            m_pass = 2;
+            *m_process << " -o " << KShellProcess::quote( m_data->getAviFile() );
+            *m_process << " -R 2," + m_data->getProjectDir() + "/tmp/divx4.log" << m_data->getParaCodec();
+            debugPass += " -R 2," + m_data->getProjectDir() + "/tmp/divx4.log" + m_data->getParaCodec() + "-o " + m_data->getAviFile();
+        }
+    } else {
+        *m_process << " -o " << KShellProcess::quote( m_data->getAviFile() );
+        *m_process << m_data->getParaCodec();
+        debugPass += m_data->getParaCodec();
+    }
+    // -x vob
+    kdDebug() << "(K3bDivXEncodingProcess)" + transcodeBin->path + " -i " + m_data->getProjectDir() +
+    "/vob -T" + m_data->getTitle() + ",-1,1" +
     m_data->getParaVideoBitrate() + "," + m_data->getKeyframes() + "," + m_data->getCrispness() +
     m_data->getParaYuv() + m_data->getParaAudioLanguage() +
-    m_data->getParaDeinterlace() + " -o " + m_data->getAviFile() +
-    " -j " + QString::number( top ) + "," + QString::number( left ) + "," + QString::number( bottom ) + "," + QString::number( right ) +
-    " -B " + QString::number( m_data->getResizeHeight() ) + "," + QString::number( m_data->getResizeWidth() ) + ",8" + debugPass << endl;
-  connect( m_process, SIGNAL( receivedStdout( KProcess*, char*, int ) ),
-	   this, SLOT( slotParseEncoding( KProcess*, char*, int ) ) );
-  connect( m_process, SIGNAL( receivedStderr( KProcess*, char*, int ) ),
-	   this, SLOT( slotParseEncoding( KProcess*, char*, int ) ) );
+    m_data->getParaDeinterlace() + " -j " + QString::number( top ) + "," + QString::number( left ) + "," + QString::number( bottom ) + 
+    "," + QString::number( right ) + " -B " + QString::number( m_data->getResizeHeight() ) + "," + QString::number( m_data->getResizeWidth() ) + ",8" + debugPass << endl;
+    
+    connect( m_process, SIGNAL( receivedStdout( KProcess*, char*, int ) ),
+             this, SLOT( slotParseEncoding( KProcess*, char*, int ) ) );
+    connect( m_process, SIGNAL( receivedStderr( KProcess*, char*, int ) ),
+             this, SLOT( slotParseEncoding( KProcess*, char*, int ) ) );
 
-  connect( m_process, SIGNAL( processExited( KProcess* ) ), this, SLOT( slotEncodingExited( KProcess* ) ) );
+    connect( m_process, SIGNAL( processExited( KProcess* ) ), this, SLOT( slotEncodingExited( KProcess* ) ) );
 
-  if ( !m_process->start( KProcess::NotifyOnExit, KProcess::AllOutput ) ) {
-    kdDebug() << "(K3bDivXEncodingProcess) Error process starting" << endl;
-  }
-  //emit started();
-  if ( m_pass == 1 ) {
-    infoMessage( i18n( "Start first pass of video encoding." ), PROCESS );
-    emit newSubTask( i18n( "Encoding video (Pass 1)" ) );
-  } else if ( m_pass == 2 ) {
-    infoMessage( i18n( "Start second pass of video encoding." ), PROCESS );
-    emit newSubTask( i18n( "Encoding video (Pass 2)" ) );
-  } else {
-    infoMessage( i18n( "Start video encoding." ), PROCESS );
-    emit newSubTask( i18n( "Encoding video" ) );
-  }
-  kdDebug() << "(K3bDivXEncodingProcess) Starting encoding." << endl;
+    if ( !m_process->start( KProcess::NotifyOnExit, KProcess::AllOutput ) ) {
+        kdDebug() << "(K3bDivXEncodingProcess) Error process starting" << endl;
+    }
+    //emit started();
+    if ( m_pass == 1 ) {
+        infoMessage( i18n( "Start first pass of video encoding." ), PROCESS );
+        emit newSubTask( i18n( "Encoding video (Pass 1)" ) );
+    } else if ( m_pass == 2 ) {
+        infoMessage( i18n( "Start second pass of video encoding." ), PROCESS );
+        emit newSubTask( i18n( "Encoding video (Pass 2)" ) );
+    } else {
+        infoMessage( i18n( "Start video encoding." ), PROCESS );
+        emit newSubTask( i18n( "Encoding video" ) );
+    }
+    kdDebug() << "(K3bDivXEncodingProcess) Starting encoding." << endl;
 }
 
 void K3bDivXEncodingProcess::copyIfos() {
-  QDir vobs( m_data->getProjectDir() + "/tmp" );
-  if ( vobs.exists() ) {
-    QStringList ifos = vobs.entryList( "*.ifo" );
-    for ( QStringList::Iterator it = ifos.begin(); it != ifos.end(); ++it ) {
-      ( *it ) = m_data->getProjectDir() + "/tmp/" + ( *it );
+    QDir tmp( m_data->getProjectDir() + "/tmp" );
+    if ( tmp.exists() ) {
+        QStringList ifos = tmp.entryList( "*.ifo" );
+        for ( QStringList::Iterator it = ifos.begin(); it != ifos.end(); ++it ) {
+            ( *it ) = m_data->getProjectDir() + "/tmp/" + ( *it );
+        }
+        KURL::List ifoList( ifos );
+        KURL dest( m_data->getProjectDir() + "/vob" );
+        // old slot: slotStartAudioProcessing
+        connect( KIO::copy( ifoList, dest, false ), SIGNAL( result( KIO::Job * ) ), this, SLOT( slotFinishedCopyIfos( KIO::Job* ) ) );
+        kdDebug() << "(K3bDivxEncodingProcess) Copy IFO files to " << dest.path() << endl;
+    } else {
+        kdDebug() << "(K3bDivXEncodingProcess) Can't find ifo files in tmp directory." << endl;
     }
-    KURL::List ifoList( ifos );
-    KURL dest( m_data->getProjectDir() + "/vob" );
-    connect( KIO::copy( ifoList, dest, false ), SIGNAL( result( KIO::Job * ) ), this, SLOT( slotStartAudioProcessing( KIO::Job* ) ) );
-    kdDebug() << "(K3bDivxEncodingProcess) Copy IFO files to " << dest.path() << endl;
-  }
 }
 
 void K3bDivXEncodingProcess::deleteIfos() {
@@ -263,21 +284,27 @@ void K3bDivXEncodingProcess::slotEncodingExited( KProcess *p ) {
       kdDebug() << "(K3bDivxEncodingProcess) Start second pass." << endl;
       slotStartEncoding();
     } else {
-      emit debuggingOutput( "videoencoding (transcode)", m_debugBuffer );
-      restoreBackupFiles();
-      if ( m_data->isShutdown() ) {
-	kdDebug() << "(K3bDivxEncodingProcess) Encoding finished. Shutting down now." << endl;
-	if ( !shutdown() ) {
-	  infoMessage( i18n( "Couldn't shutdown the system." ), ERROR );
-	  emit finished( true );
-	}
-      } else {
-	infoMessage( i18n( "Video generating successful finished." ), STATUS );
-	emit finished( true );
-      }
-      infoMessage( i18n("Video generating successfully finished."), STATUS );
-      emit debuggingOutput("videoencoding (transcode)", m_debugBuffer);
-      emit finished( true );
+        delete m_process;
+        if ( m_pass == 1 ) { // one pass mode is m_pass=0
+            kdDebug() << "(K3bDivxEncodingProcess) Start second pass." << endl;
+            slotStartEncoding();
+        } else {
+            emit debuggingOutput( "videoencoding (transcode)", m_debugBuffer );
+            restoreBackupFiles();
+            if ( m_data->isShutdown() ) {
+                kdDebug() << "(K3bDivxEncodingProcess) Encoding finished. Shutting down now." << endl;
+                if ( !shutdown() ) {
+                    infoMessage( i18n( "Couldn't shutdown the system." ), ERROR );
+                    emit finished( true );
+                }
+            } else {
+                infoMessage( i18n( "Video generating successful finished." ), STATUS );
+                emit finished( true );
+            }
+            infoMessage( i18n("Video generating successfully finished."), STATUS );
+            emit debuggingOutput("videoencoding (transcode)", m_debugBuffer);
+            emit finished( true );
+        }
     }
   }
 }
@@ -336,9 +363,9 @@ void K3bDivXEncodingProcess::slotShutdown( bool b ) {
 
 bool K3bDivXEncodingProcess::shutdown() {
   // not sure if KApplication::ShutdownModeSchedule is the one to use...
-  return kapp->requestShutDown( KApplication::ShutdownConfirmYes, 
+  return kapp->requestShutDown( KApplication::ShutdownConfirmNo, 
 				KApplication::ShutdownTypeHalt, 
-				KApplication::ShutdownModeSchedule );
+				KApplication::ShutdownModeInteractive );
 }
 
 void K3bDivXEncodingProcess::checkVobDirectory() {
--- src/videoEncoding/k3bdivxextsettings.cpp
+++ src/videoEncoding/k3bdivxextsettings.cpp
@@ -13,22 +13,6 @@
  * See the file "COPYING" for the exact licensing terms.
  */
 
-/***************************************************************************
-                          k3bdivxextsettings.cpp  -  description
-                             -------------------
-    begin                : Tue Jul 30 2002
-    copyright            : (C) 2002 by Sebastian Trueg
-    email                : trueg@informatik.uni-freiburg.de
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 
 #include "k3bdivxextsettings.h"
 #include "k3bdivxcodecdata.h"
@@ -36,24 +20,28 @@
 #include <qcheckbox.h>
 #include <qlayout.h>
 #include <qwhatsthis.h>
-#include <qsizepolicy.h>
+//#include <qsizepolicy.h>
+//#include <qhbuttongroup.h>
+//#include <qradiobutton.h>
+#include <qlabel.h>
+#include <qwhatsthis.h>
 
 #include <kdialog.h>
 #include <klocale.h>
 #include <klineedit.h>
+#include <kdebug.h>
 
-K3bDivxExtSettings::K3bDivxExtSettings(K3bDivxCodecData *data, QWidget *parent, const char *name ) : QGroupBox(parent,name) {
+K3bDivxExtSettings::K3bDivxExtSettings( K3bDivxCodecData *data, QWidget *parent, const char *name ) : QGroupBox( parent, name ) {
     m_data = data;
     setupGui();
 }
 
-K3bDivxExtSettings::~K3bDivxExtSettings(){
-}
+K3bDivxExtSettings::~K3bDivxExtSettings() {}
 
-void K3bDivxExtSettings::setupGui(){
-    setColumnLayout(0, Qt::Vertical );
-    setTitle( i18n( "Expert Settings" ) );
-    layout()->setMargin( 0 );
+void K3bDivxExtSettings::setupGui() {
+    setColumnLayout( 0, Qt::Vertical );
+    setTitle( i18n( "Expert settings" ) );
+    layout() ->setMargin( 0 );
     QGridLayout *mainLayout = new QGridLayout( layout() );
     mainLayout->setSpacing( KDialog::spacingHint() );
     mainLayout->setMargin( KDialog::marginHint() );
@@ -61,8 +49,8 @@ void K3bDivxExtSettings::setupGui(){
     m_checkShutdown = new QCheckBox( i18n("Shutdown after encoding process finished"), this );
 
     // TODO: enable me after message freeze
-//     QWhatsThis::add( m_checkShutdown, i18n("If enabled, K3b shuts the system down after encoding has finished."
-// 					   "This only works in when using Kdm.") );
+    QWhatsThis::add( m_checkShutdown, i18n("If enabled, K3b shuts the system down after encoding has finished."
+        "This only works in when using Kdm.") );
 
     m_checkWithoutAudio = new QCheckBox( i18n("Start encoding without detecting normalize parameter for audio."), this);
     m_checkOnlyFirstPass = new QCheckBox( i18n("Do only the first pass of a two-pass encoding."), this);
@@ -70,43 +58,47 @@ void K3bDivxExtSettings::setupGui(){
     m_lineTwoPassLog = new KLineEdit( m_data->getProjectDir()+"/tmp/divx4.log", this);
 
     // TODO
-    //m_checkShutdown->setEnabled( true );
     m_checkOnlyFirstPass->setEnabled( false );
     m_checkOnlySecondPass->setEnabled( false );
     m_lineTwoPassLog->setEnabled( false );
-    
-    mainLayout->addWidget( m_checkShutdown, 0, 0);
-    mainLayout->addWidget( m_checkWithoutAudio, 1, 0);
-    mainLayout->addWidget( m_checkOnlyFirstPass, 2, 0);
-    mainLayout->addWidget( m_checkOnlySecondPass, 3, 0);
-    mainLayout->addWidget( m_lineTwoPassLog, 4, 0);
-    
-    QSpacerItem* spacer = new QSpacerItem( 20, 20, QSizePolicy::Expanding, QSizePolicy::Minimum );
-    mainLayout->addItem( spacer, 5, 0);
 
-    connect( m_checkWithoutAudio, SIGNAL( stateChanged( int )), SLOT( slotNoAudio( int )));
-    connect( m_checkShutdown, SIGNAL( stateChanged( int )), SLOT( slotShutdown( int )));
+    mainLayout->addMultiCellWidget( m_checkShutdown, 0, 0, 0, 3 );
+    mainLayout->addWidget( m_checkWithoutAudio, 1, 0 );
+    mainLayout->addWidget( m_checkOnlyFirstPass, 2, 0 );
+    mainLayout->addWidget( m_checkOnlySecondPass, 3, 0 );
+    mainLayout->addMultiCellWidget( m_lineTwoPassLog, 4, 4, 0, 3 );
+    //mainLayout->addMultiCellWidget( transcodemode, 5, 5, 0, 1 );
+    //mainLayout->addMultiCellWidget( m_tcDvdModeGroup, 5, 5, 2, 2 );
+    //mainLayout->addItem( spacer, 5, 3 );
+    mainLayout->setRowStretch(5, 50);
+    
+    connect( m_checkWithoutAudio, SIGNAL( stateChanged( int ) ), SLOT( slotNoAudio( int ) ) );
+    connect( m_checkShutdown, SIGNAL( stateChanged( int ) ), SLOT( slotShutdown( int ) ) );
+    //connect( m_tcDvdModeGroup, SIGNAL( clicked( int ) ), SLOT( slotTcDvdModeGroup( int ) ) );
 }
 
-void K3bDivxExtSettings::slotUpdateView(){
-    m_lineTwoPassLog->setText( m_data->getProjectDir() + "/tmp/divx4.log");
+void K3bDivxExtSettings::slotUpdateView() {
+    m_lineTwoPassLog->setText( m_data->getProjectDir() + "/tmp/divx4.log" );
 }
-    
-void K3bDivxExtSettings::slotNoAudio( int mode ){
-    if( mode == 0 ){
+
+void K3bDivxExtSettings::slotNoAudio( int mode ) {
+    if ( mode == 0 ) {
         m_data->setNormalize( true );
     } else {
         m_data->setNormalize( false );
     }
 }
 
-void K3bDivxExtSettings::slotShutdown( int mode ){
-    if( mode == 0){
+void K3bDivxExtSettings::slotShutdown( int mode ) {
+    if ( mode == 0 ) {
         m_data->setShutdown( false );
     } else {
         m_data->setShutdown( true );
     }
 }
+
+
+
 
 #include "k3bdivxextsettings.moc"
 
