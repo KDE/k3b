21ed9ed24fe26eb1f947606e56562db61b7dd929
Sebastian Trueg <sebastian@trueg.de>
2003-05-18 18:11:26 +0000

    using K3bStdGuiItems::purpleFrame
    svn path=/trunk/kdeextragear-1/k3b/; revision=226581

--- src/rip/k3bmovieview.cpp
+++ src/rip/k3bmovieview.cpp
@@ -1,19 +1,18 @@
-/***************************************************************************
-                          k3bmovieview.cpp  -  description
-                             -------------------
-    begin                : Tue May 14 2002
-    copyright            : (C) 2002 by Sebastian Trueg
-    email                : trueg@informatik.uni-freiburg.de
- ***************************************************************************/
+/* 
+ *
+ * $Id$
+ * Copyright (C) 2003 Thomas Froescher <tfroescher@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 
 #include "k3bmovieview.h"
 #include "k3bdvdcontent.h"
@@ -23,6 +22,7 @@
 #include "k3bdvdriplistviewitem.h"
 #include <kcutlabel.h>
 #include <k3btoolbox.h>
+#include <k3bstdguiitems.h>
 
 #include <qstring.h>
 #include <qlayout.h>
@@ -63,8 +63,10 @@ K3bMovieView::K3bMovieView(QWidget *parent, const char *name )
   setupActions();
 }
 
-K3bMovieView::~K3bMovieView(){
-    delete m_tcWrapper;
+
+K3bMovieView::~K3bMovieView()
+{
+  delete m_tcWrapper;
 }
 
 void K3bMovieView::setupGUI()
@@ -76,14 +78,15 @@ void K3bMovieView::setupGUI()
 
   // header
   // ----------------------------------------------------------------------------------
-  QHBoxLayout* headerLayout = new QHBoxLayout( 0, 0, 0, "headerLayout"); 
-
-  QLabel* pixmapLabel1 = new QLabel( this, "pixmapLabel1" );
+  QFrame* headerFrame = K3bStdGuiItems::purpleFrame( this );
+  QHBoxLayout* headerLayout = new QHBoxLayout( headerFrame ); 
+  headerLayout->setMargin( 2 );  // to make sure the frame gets displayed
+  QLabel* pixmapLabel1 = new QLabel( headerFrame, "pixmapLabel1" );
   pixmapLabel1->setPixmap( QPixmap(locate( "appdata", "pics/diskinfo_left.png" )) );
   pixmapLabel1->setScaledContents( FALSE );
   headerLayout->addWidget( pixmapLabel1 );
 
-  m_labelTitle = new QLabel( i18n("Video DVD"), this, "m_labelTitle" );
+  m_labelTitle = new QLabel( i18n("Video DVD"), headerFrame, "m_labelTitle" );
   m_labelTitle->setSizePolicy( QSizePolicy( (QSizePolicy::SizeType)5, (QSizePolicy::SizeType)5, 1, 0, m_labelTitle->sizePolicy().hasHeightForWidth() ) );
   m_labelTitle->setPaletteBackgroundColor( QColor( 205, 210, 255 ) );
   QFont m_labelTitle_font( m_labelTitle->font() );
@@ -92,7 +95,7 @@ void K3bMovieView::setupGUI()
   m_labelTitle->setFont( m_labelTitle_font ); 
   headerLayout->addWidget( m_labelTitle );
 
-  QLabel* pixmapLabel2 = new QLabel( this, "pixmapLabel2" );
+  QLabel* pixmapLabel2 = new QLabel( headerFrame, "pixmapLabel2" );
   pixmapLabel2->setPixmap( QPixmap(locate( "appdata", "pics/diskinfo_dvd.png" )) );
   pixmapLabel2->setScaledContents( FALSE );
   headerLayout->addWidget( pixmapLabel2 );
@@ -123,21 +126,25 @@ void K3bMovieView::setupGUI()
   connect( m_listView, SIGNAL(selectionChanged( QListViewItem* )),
 	   this, SLOT(slotTitleSelected( QListViewItem* )) );
 
-  layout->addLayout( headerLayout );
+  layout->addWidget( headerFrame );
   layout->addLayout( toolBoxLayout );
   layout->addWidget( m_listView );  
 }
 
-void K3bMovieView::setupActions(){
-    m_actionCollection = new KActionCollection( this );
-    KAction *m_copyAction = KStdAction::copy( this, SLOT(slotRip()), m_actionCollection );
 
-    m_popupMenu = new KPopupMenu( this );
-    m_copyAction->plug( m_popupMenu );
+void K3bMovieView::setupActions()
+{
+  m_actionCollection = new KActionCollection( this );
+  KAction *m_copyAction = KStdAction::copy( this, SLOT(slotRip()), m_actionCollection );
+
+  m_popupMenu = new KPopupMenu( this );
+  m_copyAction->plug( m_popupMenu );
 }
 
-void K3bMovieView::setDevice( K3bDevice* device ){
-    m_device = device;
+
+void K3bMovieView::setDevice( K3bDevice* device )
+{
+  m_device = device;
 }
 
 void K3bMovieView::reload()
@@ -164,133 +171,142 @@ void K3bMovieView::reload()
   m_tcWrapper->checkDvdContent( m_device );
 }
 
-QString K3bMovieView::filterAudioList( QStringList *al ){
-    QString result("");
-    for ( QStringList::Iterator it = al->begin(); it != al->end(); ++it ) {
-        QStringList lang = QStringList::split(" ", (*it) );
-        if( lang[1].contains("bit") || lang[1].contains("drc") ){
-            lang[1] = "??";
-        }
-        if( !result.contains( lang[1] ) ){
-        result += lang[1] +", "; // too much space for all info + " (" + lang[0] + "/" + lang.last() +"), ";
-        }
-        kdDebug() << "(K3bMovieView) Parse audio tracks for language: " << result << endl;
+QString K3bMovieView::filterAudioList( QStringList *al )
+{
+  QString result("");
+  for ( QStringList::Iterator it = al->begin(); it != al->end(); ++it ) {
+    QStringList lang = QStringList::split(" ", (*it) );
+    if( lang[1].contains("bit") || lang[1].contains("drc") ){
+      lang[1] = "??";
     }
-    return result.left( result.length()-2);
+    if( !result.contains( lang[1] ) ){
+      result += lang[1] +", "; // too much space for all info + " (" + lang[0] + "/" + lang.last() +"), ";
+    }
+    kdDebug() << "(K3bMovieView) Parse audio tracks for language: " << result << endl;
+  }
+  return result.left( result.length()-2);
 }
 
 // ------------------------------------------
 // slots
 // ------------------------------------------
 
-void K3bMovieView::slotDvdChecked( bool successful ){
-    m_dvdTitles.clear();
+void K3bMovieView::slotDvdChecked( bool successful )
+{
+  m_dvdTitles.clear();
 
-    if( successful ){
-        K3bDvdRipListViewItem *longestTitle;
-        long maxFrames = 0;
-        m_dvdTitles = m_tcWrapper->getDvdTitles();
-        m_listView->clear();
-        QStringList existingTitlesets;
-        for( unsigned int i= 0; i < m_dvdTitles.count();  i++){
-            K3bDvdContent *title = &(m_dvdTitles[ i ]);
-            int t=title->getTitleSet();
-            QString strTitleset = QString::number( t );
-            if ( t < 10 )
-                strTitleset = "0" + strTitleset;
-            QString mainEntryAndKey = i18n("Titleset %1").arg( strTitleset );
-            kdDebug() << "(K3bMovieView) Titleset: " << QString::number( title->getTitleSet()) << " Title: " << QString::number( title->getTitleNumber() ) << endl;
-            if( !existingTitlesets.contains( QString::number( title->getTitleSet()) ) ){
-                existingTitlesets << QString::number( title->getTitleSet());
-                K3bDvdRipListViewItem *item = new K3bDvdRipListViewItem( m_listView, mainEntryAndKey );
-                item->setExpandable( true );
-                item->setOpen( false );
-                item->setHiddenTitle( -1 );
-                item->setTitleSet( true );
-           }
-           K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), 
-									  i18n("Title %1").arg(title->getTitleNumber() ),
-									  title->getStrTime(), 
-									  filterAudioList( title->getAudioList() ), 
-									  QString::number( title->getMaxChapters() ), 
-									  title->getAngles()->join(",") );
-           titleItem->setHiddenTitle( title->getTitleNumber( ));
-           if( title->getFrames() > maxFrames ){
-               maxFrames = title->getFrames();
-               longestTitle = titleItem;
-           }
-/*
-                if( existingTitlesets.contains( QString::number( title->getTitleSet()) ) ){
-                K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), i18n("Title %1").arg(title->getTitleNumber() ),
-                title->getStrTime(), filterAudioList( title->getAudioList() ), QString::number( title->getMaxChapters() ), title->getAngles()->join(",") );
-                titleItem->setHiddenTitle( title->getTitleNumber( ));
-            } else {
-                existingTitlesets << QString::number( title->getTitleSet());
-                K3bDvdRipListViewItem *item = new K3bDvdRipListViewItem( m_listView, mainEntryAndKey );
-                item->setExpandable( true );
-                item->setOpen( false );
-                item->setHiddenTitle( -1 );
-                item->setTitleSet( true );
-                K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), i18n("Title %1").arg(title->getTitleNumber() ),
-                title->getStrTime(), filterAudioList( title->getAudioList() ), QString::number( title->getMaxChapters() ), title->getAngles()->join(",") );
-                titleItem->setHiddenTitle( title->getTitleNumber( ));
-            }
-*/
-        }
-
-        QWidget::show();
-        longestTitle->parent()->setOpen( true );
-        m_listView->setSelected( longestTitle, true );
-    } else {
-        // error during parsing
-      emit notSupportedDisc( m_device->devicename() );
+  if( successful ){
+    K3bDvdRipListViewItem *longestTitle;
+    long maxFrames = 0;
+    m_dvdTitles = m_tcWrapper->getDvdTitles();
+    m_listView->clear();
+    QStringList existingTitlesets;
+    for( unsigned int i= 0; i < m_dvdTitles.count();  i++){
+      K3bDvdContent *title = &(m_dvdTitles[ i ]);
+      int t=title->getTitleSet();
+      QString strTitleset = QString::number( t );
+      if ( t < 10 )
+	strTitleset = "0" + strTitleset;
+      QString mainEntryAndKey = i18n("Titleset %1").arg( strTitleset );
+      kdDebug() << "(K3bMovieView) Titleset: " << QString::number( title->getTitleSet()) << " Title: " << QString::number( title->getTitleNumber() ) << endl;
+      if( !existingTitlesets.contains( QString::number( title->getTitleSet()) ) ){
+	existingTitlesets << QString::number( title->getTitleSet());
+	K3bDvdRipListViewItem *item = new K3bDvdRipListViewItem( m_listView, mainEntryAndKey );
+	item->setExpandable( true );
+	item->setOpen( false );
+	item->setHiddenTitle( -1 );
+	item->setTitleSet( true );
+      }
+      K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), 
+								     i18n("Title %1").arg(title->getTitleNumber() ),
+								     title->getStrTime(), 
+								     filterAudioList( title->getAudioList() ), 
+								     QString::number( title->getMaxChapters() ), 
+								     title->getAngles()->join(",") );
+      titleItem->setHiddenTitle( title->getTitleNumber( ));
+      if( title->getFrames() > maxFrames ){
+	maxFrames = title->getFrames();
+	longestTitle = titleItem;
+      }
+      /*
+	if( existingTitlesets.contains( QString::number( title->getTitleSet()) ) ){
+	K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), i18n("Title %1").arg(title->getTitleNumber() ),
+	title->getStrTime(), filterAudioList( title->getAudioList() ), QString::number( title->getMaxChapters() ), title->getAngles()->join(",") );
+	titleItem->setHiddenTitle( title->getTitleNumber( ));
+	} else {
+	existingTitlesets << QString::number( title->getTitleSet());
+	K3bDvdRipListViewItem *item = new K3bDvdRipListViewItem( m_listView, mainEntryAndKey );
+	item->setExpandable( true );
+	item->setOpen( false );
+	item->setHiddenTitle( -1 );
+	item->setTitleSet( true );
+	K3bDvdRipListViewItem *titleItem =  new K3bDvdRipListViewItem( m_listView->findItem( mainEntryAndKey, 0), i18n("Title %1").arg(title->getTitleNumber() ),
+	title->getStrTime(), filterAudioList( title->getAudioList() ), QString::number( title->getMaxChapters() ), title->getAngles()->join(",") );
+	titleItem->setHiddenTitle( title->getTitleNumber( ));
+	}
+      */
     }
-}
 
-void K3bMovieView::slotTitleSelected( QListViewItem *item){
-    int title = (dynamic_cast<K3bDvdRipListViewItem*>(item) )->getHiddenTitle();
-    if( title > 0 ){
-        DvdTitle::Iterator dvd;
-        dvd = m_dvdTitles.at( title-1 );
-        QString ext = "";
-        if( (*dvd).getStrAspectAnamorph().length() > 1 ){
-            ext = "(" + (*dvd).getStrAspectAnamorph();
-        }
-        if( (*dvd).getStrAspectExtension().length() > 1 ){
-           ( ext.length() > 1 ) ? ext += ", " : ext += "(";
-           ext += (*dvd).getStrAspectExtension();
-        }
-        if( ext.length() > 1){
-            ext = ext + ")";
-        }
-        m_labelDvdInfo->setText( i18n("Title %1 (Angle(s) %2)\nMoviedata - TV Norm: %3, Time: %4 hours, Frames: %5, FPS: %6\n\
-Moviedata - Aspect Ratio %7 %8").arg(
-        (*dvd).getTitleNumber()).arg((*dvd).getAngles()->join(",")).arg((*dvd).getMode()).arg((*dvd).getStrTime()).arg((*dvd).getStrFrames()).arg(
-        (*dvd).getStrFramerate()).arg((*dvd).getStrAspect()).arg(ext) );
-    }
-}
-
-void K3bMovieView::slotNotSupportedDisc(){
+    QWidget::show();
+    longestTitle->parent()->setOpen( true );
+    m_listView->setSelected( longestTitle, true );
+  } else {
+    // error during parsing
     emit notSupportedDisc( m_device->devicename() );
+  }
 }
 
-void K3bMovieView::slotUpdateInfoDialog( int i ){
-   m_fetchingInfoLabel->setText( "<"+QString::number( i )+">" );
-}
-
-void K3bMovieView::slotRip(){
-    K3bDvdRipperWidget ripWidget( m_device->devicename(), this, "dvdrip");
+void K3bMovieView::slotTitleSelected( QListViewItem *item)
+{
+  int title = (dynamic_cast<K3bDvdRipListViewItem*>(item) )->getHiddenTitle();
+  if( title > 0 ){
     DvdTitle::Iterator dvd;
-    int title = m_ripTitle->getHiddenTitle( );
-    kdDebug() << QString::number(title) << " Title" << endl;
-    DvdTitle toRipTitles;
-    // clear old angle selection if already used
     dvd = m_dvdTitles.at( title-1 );
-    toRipTitles.append( *dvd );
-    ripWidget.init( toRipTitles );
-    ripWidget.exec();
+    QString ext = "";
+    if( (*dvd).getStrAspectAnamorph().length() > 1 ){
+      ext = "(" + (*dvd).getStrAspectAnamorph();
+    }
+    if( (*dvd).getStrAspectExtension().length() > 1 ){
+      ( ext.length() > 1 ) ? ext += ", " : ext += "(";
+      ext += (*dvd).getStrAspectExtension();
+    }
+    if( ext.length() > 1){
+      ext = ext + ")";
+    }
+    m_labelDvdInfo->setText( i18n("Title %1 (Angle(s) %2)\nMoviedata - TV Norm: %3, Time: %4 hours, Frames: %5, FPS: %6\n\
+Moviedata - Aspect Ratio %7 %8").arg(
+				     (*dvd).getTitleNumber()).arg((*dvd).getAngles()->join(",")).arg((*dvd).getMode()).arg((*dvd).getStrTime()).arg((*dvd).getStrFrames()).arg(
+																					       (*dvd).getStrFramerate()).arg((*dvd).getStrAspect()).arg(ext) );
+  }
 }
-void K3bMovieView::slotContextMenu( KListView*, QListViewItem *lvi, const QPoint& p ){
+
+void K3bMovieView::slotNotSupportedDisc()
+{
+  emit notSupportedDisc( m_device->devicename() );
+}
+
+void K3bMovieView::slotUpdateInfoDialog( int i )
+{
+  m_fetchingInfoLabel->setText( "<"+QString::number( i )+">" );
+}
+
+void K3bMovieView::slotRip()
+{
+  K3bDvdRipperWidget ripWidget( m_device->devicename(), this, "dvdrip");
+  DvdTitle::Iterator dvd;
+  int title = m_ripTitle->getHiddenTitle( );
+  kdDebug() << QString::number(title) << " Title" << endl;
+  DvdTitle toRipTitles;
+  // clear old angle selection if already used
+  dvd = m_dvdTitles.at( title-1 );
+  toRipTitles.append( *dvd );
+  ripWidget.init( toRipTitles );
+  ripWidget.exec();
+}
+
+
+void K3bMovieView::slotContextMenu( KListView*, QListViewItem *lvi, const QPoint& p )
+{
   m_ripTitle = dynamic_cast<K3bDvdRipListViewItem*>(lvi);
   m_popupMenu->exec(p);
 }
