7ca66cee41843ac69580cf84f57d24dcde4f5854
Sebastian Trueg <sebastian@trueg.de>
2003-07-26 19:08:18 +0000

    *** empty log message ***
    svn path=/trunk/kdeextragear-1/k3b/; revision=239305

---
+++ src/projects/audiocd/input/k3baudiomodulefactory.cpp
@@ -0,0 +1,90 @@
+/* 
+ *
+ * $Id$
+ * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2003 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include <config.h>
+
+#include "k3baudiomodulefactory.h"
+#include "mp3/k3bmp3module.h"
+#include "wave/k3bwavemodule.h"
+
+#ifdef OGG_VORBIS
+#include "ogg/k3boggvorbismodule.h"
+#endif
+
+#include <qstring.h>
+
+#include <kurl.h>
+#include <kfileitem.h>
+#include <kdebug.h>
+
+
+K3bAudioModuleFactory::K3bAudioModuleFactory()
+  : QObject()
+{
+  // create an instance of all available modules 
+  // (This should be plugins in the future)
+
+  m_modules.append( new K3bWaveModule( this ) );
+
+  m_modules.append( new K3bMp3Module( this ) );
+
+#ifdef OGG_VORBIS
+  m_modules.append( new K3bOggVorbisModule( this ) );
+#endif
+}
+
+
+K3bAudioModuleFactory::~K3bAudioModuleFactory()
+{
+  // QObject should make sure the modules get deleted
+}
+
+K3bAudioModuleFactory* K3bAudioModuleFactory::self()
+{
+  static K3bAudioModuleFactory* factoryInstance = 0;
+  if( factoryInstance == 0 ) {
+    factoryInstance = new K3bAudioModuleFactory();
+  }
+
+  return factoryInstance;
+}
+
+
+K3bAudioModule* K3bAudioModuleFactory::createModule( const KURL& url )
+{
+  for( QPtrListIterator<K3bAudioModule> it( m_modules ); it.current(); ++it ) {
+    if( it.current()->canDecode( url ) ) {
+      return it.current();
+    }
+  }
+
+  kdDebug() << "(K3bAudioModuleFactory) No K3bAudioModule available." << endl;
+  return 0;
+}
+
+
+bool K3bAudioModuleFactory::moduleAvailable( const KURL& url )
+{
+  for( QPtrListIterator<K3bAudioModule> it( m_modules ); it.current(); ++it ) {
+    if( it.current()->canDecode( url ) ) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+
+#include "k3baudiomodulefactory.moc"
