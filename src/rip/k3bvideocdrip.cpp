0315335da313f3b707bbb49a8db50364676d8338
Gregory Mokhin <mokhin@bog.msu.ru>
2003-11-03 15:54:55 +0000

    i18n fix: please don't split messages - it breaks translations; use  form of arguments to i18n() only for numerical arguments -- mok
    svn path=/trunk/kdeextragear-1/k3b/; revision=264243

--- src/rip/k3bvideocdrip.cpp
+++ src/rip/k3bvideocdrip.cpp
@@ -96,7 +96,7 @@ void K3bVideoCdRip::vcdxRip()
     if ( !bin ) {
         kdDebug() << "(K3bVideoCdRip) could not find vcdxrip executable" << endl;
         emit infoMessage( i18n("Could not find %1 executable.").arg("vcdxrip"), K3bJob::ERROR );
-        emit infoMessage(i18n( "To %1 VideoCD's you must install VcdImager >= 0.7.12." ).arg( i18n("rip") ), K3bJob::INFO );
+        emit infoMessage(i18n( "To rip VideoCD's you must install VcdImager >= 0.7.12." ), K3bJob::INFO );
         emit infoMessage(i18n( "You can find this on your distribution disks or download it from http://www.vcdimager.org" ),K3bJob::INFO );
         cancelAll();
         emit finished( false );
