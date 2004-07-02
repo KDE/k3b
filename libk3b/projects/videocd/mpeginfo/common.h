53fe2fe15faa35a226f5555c127b3f99f1d5dc7a
Sebastian Trueg <sebastian@trueg.de>
2004-07-02 16:48:12 +0000

    new k3b gfilestructure
    svn path=/trunk/kdeextragear-1/k3b/; revision=325373

---
+++ libk3b/projects/videocd/mpeginfo/common.h
@@ -0,0 +1,225 @@
+/*
+*
+* $Id$
+* Copyright (C) 2003 Christian Kvasny <chris@k3b.org>
+*
+* This file is part of the K3b project.
+* Copyright (C) 1998-2004 Sebastian Trueg <trueg@k3b.org>
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 2 of the License, or
+* (at your option) any later version.
+* See the file "COPYING" for the exact licensing terms.
+*/
+
+#ifndef CoMmOn
+#define CoMmOn
+
+
+// SYSTEM INCLUDE FILES
+
+#include <stdio.h>
+
+/*
+ * for fopen, fprintf, printf, fread...
+ */
+#include <string.h>
+
+/*
+ * fseeko(), ftello() conforms to SUS 2 for LFS suppport
+ * Also needed for strcpy, strlen..
+ */
+#include <sys/types.h>
+
+
+
+// if you don't have the signal system call (Windows for ex.)
+// then uncomment the next line
+//#define NOSIGNAL_H 1
+
+#define mpeg_SILENT  0
+#define mpeg_NORMAL  1
+#define mpeg_VERBOSE 2
+
+// packlength is variable since mpeg2
+//#define PACKlength 12
+
+#define PaddingPkt   0xBE
+#define VideoPkt     0xE0
+#define AudioPkt  	 0xC0
+#define SystemPkt    0xBB
+
+
+#define mpeg_AUDIO     1
+#define mpeg_VIDEO     2
+#define mpeg_SYSTEM    3
+#define mpeg_UNKNOWN   4
+#define mpeg_EMPTY     5
+#define mpeg_TRANSPORT 6
+
+// #define BUFFERSIZE   512
+#define BUFFERSIZE   16384 
+//16k buffer
+//#define COPYBUFFERSIZE 512
+//#define COPYBUFFERSIZE 65536
+//64k buffer
+#define COPYBUFFERSIZE 2097152 
+//2meg buffer
+
+//#define ENABLE_OPTIMIZATION 1
+// this is used to calculate time
+// used by 1)printf 2)mpeg::GetByte 3) mpegOut::Copy
+#ifdef ENABLE_OPTIMIZATION
+
+#define MAIN 0
+#define PRINTF 1
+#define GETBYTE 2
+#define COPY 3
+#define SELF 4
+#define N_FUNCTIONS 5
+
+double Clock_Now();
+void AddTime( double timestart, int function );
+extern double functions_cummulated_time[ N_FUNCTIONS ];
+void init_cummulated_time();
+void PrintTime();
+
+#define START_CLOCK double StartOfClock = Clock_Now();
+#define STOP_CLOCK(x) AddTime(StartOfClock,x);
+extern double MainClockStart;
+#else
+#define START_CLOCK ;
+#define STOP_CLOCK(x) ;
+#endif
+
+
+
+#define MAX_ID3_GENRE 148
+extern const char *genre[ MAX_ID3_GENRE ];
+
+typedef unsigned char byte;
+typedef unsigned char marker;
+typedef const char* c_char;
+
+typedef struct
+{
+    char name[ 30 ];
+    char artist[ 30 ];
+    char album [ 30 ];
+    char year [ 4 ];
+    char comment [ 30 ];
+    unsigned char genre;
+}
+id3;
+
+typedef struct
+{
+    int mpeg_ver;
+    int layer;
+    int protect;
+    int bitrate;
+    float byte_rate;
+    int sampling_rate;
+    int mode;
+    int padding;
+    int modext;
+    int emphasis_index;
+    bool copyright;
+    bool original;
+    double duration;
+    int frame_length;
+    id3* tag;
+    off_t first_frame_offset;
+}
+mpgtx_audio;
+
+typedef struct
+{
+    unsigned long hsize, vsize;
+    double frame_rate;
+    unsigned long bitrate;
+    double duration;
+    byte aspect_ratio;
+    byte* video_header;
+    int video_header_size;
+
+    off_t first_gop_offset;
+}
+mpgtx_video;
+
+typedef struct
+{
+    byte* video_system_header;
+    off_t video_system_header_length;
+
+    byte* audio_system_header;
+    off_t audio_system_header_length;
+
+    byte* first_video_packet;
+    off_t first_video_packet_length;
+    //initial timestamp
+    double initial_TS;
+    unsigned long muxrate;
+}
+mpgtx_system;
+
+
+typedef struct
+{
+    bool progressive;
+    byte chroma_format;
+    bool low_delay;
+}
+sequence_ext;
+
+typedef struct
+{
+    byte video_format;
+    byte colour_prim;
+    byte transfer_char;
+    byte matrix_coef;
+    unsigned long h_display_size;
+    unsigned long v_display_size;
+}
+display_ext;
+
+typedef struct
+{
+    char* ud;
+    int ud_length;
+}
+user_data;
+
+
+/*  The following Macros ensure compatibility for glibc < 2.2+ and kernel < 2.4
+    It also gives opportunity to support large files (>4 Go) such as DVDs
+	_OFF_d and _OFF_x are used in format strings of printf. They refer to the off_t type
+	and substitute to %lld %llx resp, with large file support and %ld %lx otherwise.
+ 
+	FSEEK substitutes to the new fseeko function if possible or becomes standard fseek otherwise
+	FTELL does the same with ftello and ftell
+ 
+*/
+
+
+#ifdef _LARGEFILE_SOURCE
+#ifdef _MACOSX
+	#define   _OFF_d   "%qd"
+	#define   _OFF_x   "%qx"
+#else
+	#define   _OFF_d   "%lld"
+ #define   _OFF_x   "%llx"
+#endif
+	#define   FSEEK    fseeko
+	#define   FTELL    ftello
+#else
+	#define   _OFF_d   "%ld"
+	#define   _OFF_x   "%lx"
+	#define   FSEEK    fseek
+	#define   FTELL    ftell
+#endif
+
+
+#endif //CoMmOn
+
---
+++ libk3b/projects/videocd/mpeginfo/mpeg.h
@@ -0,0 +1,284 @@
+/*
+ *
+ * $Id$
+ * Copyright (C) 2003 Christian Kvasny <chris@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2004 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef MpEg
+#define MpEg
+
+#include "common.h"
+
+typedef struct
+{
+    // this stores the junk header of mpeg files, in case someone needs it later
+    off_t size;
+    byte *buf;
+
+}
+header_buf;
+
+// return a pointer to the junk
+header_buf *readHeader( FILE *myMpegfile, off_t offset, int rw );
+
+typedef int PID;
+
+class mpeg_descriptors
+{
+    public:
+        mpeg_descriptors()
+        {
+            video_coding_version = -1;
+            audio_coding_version = -1;
+            CA = -1;
+            CA_PID = -1;
+            lang_audio_type = -1;
+            languages = 0;
+            copyright = false;
+            n_unhandled_desc = 0;
+        }
+        ~mpeg_descriptors()
+        {
+            if ( languages )
+                delete[] languages;
+        }
+        void PrintInfos( const char* prefix );
+        int video_coding_version;
+        int audio_coding_version;
+        int CA;
+        PID CA_PID;
+        int lang_audio_type;
+        char* languages;
+        bool copyright;
+        byte n_unhandled_desc;
+};
+
+
+typedef struct ES_t
+{
+    PID pid;
+    byte type;
+    ES_t *next;
+    mpeg_descriptors descs;
+    FILE* demuxFile;
+    bool demuxFileOk;
+}
+EStream;
+
+
+typedef struct t_prog
+{
+    EStream *TStreams;
+    int nstreams;
+    int prog_num;
+    mpeg_descriptors descs;
+}
+program;
+
+
+
+class mpeg;
+
+//#define MAX_PROGS 100
+
+//helper class that carries transport stream infos
+class transport
+{
+        friend class mpeg;
+        friend class demuxer;
+    public :
+        transport();
+        ~transport();
+
+    protected:
+        void delete_programs();
+        void delete_ES( EStream* stream );
+        void PrintInfos();
+        program* programs;
+        int n_progs;
+        int n_audio_streams;
+        int n_video_streams;
+        int n_other_streams;
+        PID network_PID;
+
+        PID* PMT_PIDs;
+        int n_PMT_PIDs;
+        int read_pmts;
+};
+
+class mpeg
+{
+        friend class mpegSystemOut;
+        friend class demuxer;
+    protected:
+        // disallow basic constructors
+        mpeg()
+        {}
+        ;
+
+        // Functions
+
+        //  General ToolBox
+
+        byte GetByte( off_t offset );
+        byte bdGetByte( off_t offset );
+        bool EnsureMPEG( off_t offset, marker mark );
+        double ReadTS ( off_t offset );
+        double ReadTSMpeg2( off_t offset );
+        long ReadPACKMuxRate( off_t offset );
+
+        // find any marker
+        off_t FindNextMarker( off_t from );
+        // find any marker, change mark accordingly
+        off_t FindNextMarker( off_t from, marker* mark );
+        off_t bdFindNextMarker( off_t from, marker* mark );
+        // find 0x00 00 01 mark
+        void print_all_ts( byte kind );
+    public:
+        mpeg( const char* filename, int verbosity = mpeg_SILENT );
+        ~mpeg();
+
+        //  General ToolBox
+
+        int MpegVersion();
+        void PrintInfos();
+        bool has_audio()
+        {
+            return HasAudio;
+        }
+        bool has_video()
+        {
+            return HasVideo;
+        }
+        off_t Size()
+        {
+            return FileSize;
+        }
+        const char* Name()
+        {
+            return FileName;
+        }
+        int Version()
+        {
+            return mpeg_version;
+        }
+        bool Match( mpeg* peer );
+        float Duration();
+        byte Byte( off_t offset );
+
+
+        off_t FindNextMarker( off_t from, marker mark );
+        off_t bdFindNextMarker( off_t from, marker mark );
+
+        unsigned short int GetSize( off_t offset );
+
+        void SecsToHMS( char* HMS, float duration );
+
+        //  Audio ToolBox
+        bool ParseAudio( off_t myoffset );
+        bool ParseID3();
+        bool PrintID3();
+
+        off_t FindMatchingAudio( off_t myoffset );
+        off_t bdFindMatchingAudio( off_t myoffset );
+        bool MatchAudio( off_t myoffset );
+        //  Video ToolBox
+        bool ParseVideo( off_t myoffset );
+        bool ParseExtension( off_t myoffset );
+        bool ParseSequenceExt( off_t myoffset );
+        bool ParseSequenceDisplayExt( off_t myoffset );
+        bool ParseUserData( off_t myoffset );
+        //  System ToolBox
+        bool ParseSystem();
+        bool ParseSystemPacket( off_t startofpacket, off_t startofpack );
+        bool ParseSystemHeader( off_t myoffset );
+        off_t SkipPacketHeader( off_t myoffset );
+
+        void ParseFramesInGOP( off_t offset );
+        bool ParseRIFF();
+        bool ParseTransportStream( off_t offset );
+        void ParseCAT( off_t offset );
+        void ParsePAT( off_t offset );
+        void ParsePMT( off_t offset );
+        off_t ParseDescriptor( off_t offset, mpeg_descriptors* target );
+        void DescCA( off_t offset, mpeg_descriptors* target );
+        void DescLang( off_t offset, mpeg_descriptors* target );
+
+        long Read12bitLength( off_t offset );
+        long Read2Bytes( off_t offset );
+        long ReadPID( off_t offset );
+
+        PID NextTrPacket( off_t* offset, off_t* payload_start, off_t* payload_end );
+
+        // file handle (I hate iostream and I can't use read/write on windows)
+        FILE* MpegFile;
+
+        // how much we speak
+        int Verboseness;
+
+        // mpeg Internals to be filled by constructor
+        bool HasAudio, HasVideo;
+        bool composite, editable;
+
+        off_t FileSize;
+
+        char* FileName;
+        int MpegType;
+
+        // audio internals
+        mpgtx_audio* Audio;
+        byte n_audio;
+        // video internals
+        mpgtx_video* Video;
+        byte n_video;
+        // system internals
+        mpgtx_system* System;
+
+        // transport stream internals
+        transport* Transport;
+
+        // extensions
+        sequence_ext* SExt;
+        display_ext* DExt;
+        user_data* UData;
+
+        // start with id3v2 tag
+        bool start_with_id3;
+        // for mpeg 1/ mpeg2
+        byte mpeg_version;
+
+        // for GetByte
+
+        off_t buffstart;
+        off_t buffend;
+        byte* buffer;
+
+};
+
+
+typedef struct
+{
+    char* file;
+    off_t from;
+    float sfrom;
+    off_t to;
+    float sto;
+    bool from_included;
+    bool to_included;
+    bool until_file_size;
+    bool unit_is_second;
+    mpeg* mpegfile;
+}
+chunk;
+
+#endif //MpEg
+
