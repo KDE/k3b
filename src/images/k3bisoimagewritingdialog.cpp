5d9ee32961399b9d0298afd7463af727b311e670
Sebastian Trueg <sebastian@trueg.de>
2004-07-04 10:41:53 +0000

    include fixes
    svn path=/branches/k3b_0_11_branch/kdeextragear-1/k3b/; revision=325705

--- src/images/k3bisoimagewritingdialog.cpp
+++ src/images/k3bisoimagewritingdialog.cpp
@@ -23,7 +23,7 @@
 #include <k3bburnprogressdialog.h>
 #include <kcutlabel.h>
 #include <k3bstdguiitems.h>
-#include <tools/k3bmd5job.h>
+#include <k3bmd5job.h>
 #include <k3bglobals.h>
 #include <k3bwritingmodewidget.h>
 #include <k3bcore.h>
