662b2ca4b66623ff54c6d7e2065e235b8c6ff275
Adriaan de Groot <groot@kde.org>
2004-07-11 18:02:27 +0000

    Merge from downstream: FBSD apparently has both VIDEO_TS and video_ts, so don't check for the collision.
    svn path=/trunk/kdeextragear-1/k3b/; revision=328018

--- src/rip/k3bdvdrippingprocess.cpp
+++ src/rip/k3bdvdrippingprocess.cpp
@@ -301,10 +301,13 @@ void K3bDvdRippingProcess::slotPreProcessingDvd() {
 
     QString video;
     QDir video_ts( m_mountPoint + "/VIDEO_TS");
+#ifndef FreeBSD
+// on FreeBSD both /VIDEO_TS and /video_ts exist on dvd devices ?!?
     if( video_ts.exists() ) {
         m_udfMount = true;
         kdDebug() << "(K3bDvdRippingProcess) <" << m_mountPoint << "> has UDF filesystem." << endl;
     }
+#endif
     video_ts.setPath( m_mountPoint + "/video_ts");
     if( !video_ts.exists() && !m_udfMount){
         m_preProcessingFailed = true;
