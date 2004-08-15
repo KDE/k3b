7f664eb584a533f7aeb9de1dd9609b5f653dd3a3
Sebastian Trueg <sebastian@trueg.de>
2004-08-15 10:41:42 +0000

    fix in device search path saving: don't save a path twice
    svn path=/branches/k3b_0_11_branch/kdeextragear-1/k3b/; revision=338070

--- src/device/k3bdevicemanager.cpp
+++ src/device/k3bdevicemanager.cpp
@@ -390,12 +390,18 @@ bool K3bCdDevice::DeviceManager::saveConfig( KConfig* c )
 
   c->setGroup( "Devices" );
   QStringList deviceSearchPath = c->readListEntry( "device_search_path" );
+  // remove duplicate entries (caused by buggy old implementations)
+  QStringList saveDeviceSearchPath;
+  for( QStringList::iterator it = deviceSearchPath.begin(); it != deviceSearchPath.end(); ++it )
+    if( !saveDeviceSearchPath.contains( *it ) )
+      saveDeviceSearchPath.append( *it );
 
   for( QPtrListIterator<K3bCdDevice::CdDevice> it( d->allDevices ); *it; ++it ) {
     K3bCdDevice::CdDevice* dev = *it;
 
     // update device search path
-    deviceSearchPath.append( dev->blockDeviceName() );
+    if( !saveDeviceSearchPath.contains( dev->blockDeviceName() ) )
+      saveDeviceSearchPath.append( dev->blockDeviceName() );
 
     // save the device type settings
     QString configEntryName = dev->vendor() + " " + dev->description();
@@ -412,7 +418,7 @@ bool K3bCdDevice::DeviceManager::saveConfig( KConfig* c )
     c->writeEntry( configEntryName, list );
   }
 
-  c->writeEntry( "device_search_path", deviceSearchPath );
+  c->writeEntry( "device_search_path", saveDeviceSearchPath );
 
   c->sync();
 
