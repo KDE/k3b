bd2383e12a4d544ce8b0a462a822ce3671ce1e3f
Sebastian Trueg <sebastian@trueg.de>
2004-12-15 17:08:47 +0000

    0.11.19cvs
    svn path=/branches/k3b_0_11_branch/kdeextragear-1/k3b/; revision=370808

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ static KCmdLineOptions options[] =
 int main(int argc, char *argv[]) {
 
     KAboutData aboutData( "k3b", I18N_NOOP("K3b"),
-                          "0.11.18cvs", description, KAboutData::License_GPL,
+                          "0.11.19cvs", description, KAboutData::License_GPL,
                           I18N_NOOP("(c) 1999 - 2004, Sebastian Trüg and the K3b Team"), 0, "http://www.k3b.org" );
     aboutData.addAuthor("Sebastian Trüg",I18N_NOOP("Maintainer"), "trueg@k3b.org");
     aboutData.addAuthor("Thomas Froescher",I18N_NOOP("Video-ripping and encoding"), "tfroescher@k3b.org");
