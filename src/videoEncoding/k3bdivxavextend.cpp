9764d73e39feb504991f720135a108e3d8c31f88
Sebastian Trueg <sebastian@trueg.de>
2005-04-09 20:16:17 +0000

    Properly fixed the string problems. No backslashes anymore.
    svn path=/trunk/kdeextragear-1/k3b/; revision=404448

--- src/videoEncoding/k3bdivxavextend.cpp
+++ src/videoEncoding/k3bdivxavextend.cpp
@@ -37,8 +37,10 @@
 K3bDivxAVExtend::K3bDivxAVExtend( K3bDivxCodecData *data, QWidget *parent, const char *name ) : QGroupBox( parent,name) {
      m_data =  data;
      m_wrongsettings = i18n("Wrong setting");
-     m_dilError = i18n("The deinterlace filter YUVDeinterlaceMMX doesn't work with RGB mode. You must enable using YUV colorspace.");
-     m_smartError = i18n("The deinterlace filter SmartDeinterlace doesn't work with YUV colorspace. It only works with RGB. You must disable using YUV colorspace.");
+     m_dilError = i18n("The deinterlace filter YUVDeinterlaceMMX doesn't work with RGB mode. "
+		       "You must enable using YUV colorspace.");
+     m_smartError = i18n("The deinterlace filter SmartDeinterlace doesn't work with YUV "
+			 "colorspace. It only works with RGB. You must disable using YUV colorspace.");
      setupGui();
 }
 
@@ -54,29 +56,42 @@ void K3bDivxAVExtend::setupGui(){
     mainLayout->setMargin( KDialog::marginHint() );
 
     QLabel *keyframes = new QLabel( i18n("Keyframes:"), this );
-    QString wt_keyframes( i18n("Keyframes (same as I-frames) are \"full\" pictures of an MPEG-stream. They are similar to a simple JPEG and have the best quality \
-in a video. All pictures following are only stored as differences to a keyframe. Here you can set the maximum distance between two keyframes. In most cases this will \
-rarely be used, as the codec will automatically insert a keyframe on each scene change. Default is 300."));
+    QString wt_keyframes( i18n("Keyframes (same as I-frames) are \"full\" pictures of an MPEG-stream. "
+			       "They are similar to a simple JPEG and have the best quality "
+			       "in a video. All pictures following are only stored as differences to a "
+			       "keyframe. Here you can set the maximum distance between two keyframes. "
+			       "In most cases this will rarely be used, as the codec will automatically "
+			       "insert a keyframe on each scene change. Default is 300."));
     QWhatsThis::add( keyframes, wt_keyframes);
     QLabel *crispness = new QLabel( i18n("Crispness:"), this );
-    // xgettext:no-c-format
-    QString wt_crispness( i18n("Here you can smooth the video during the encoding process. It is better to leave crispness at 100% for encoding and smooth \
-the video during playback instead."));
+    QString wt_crispness( i18n("Here you can smooth the video during the encoding process. "
+			       "It is better to leave crispness at 100% for encoding and smooth "
+			       "the video during playback instead."));
     QWhatsThis::add( crispness, wt_crispness);
     //QLabel *audiogain = new QLabel( i18n("Increase audio volume by:"), this );
     QLabel *language = new QLabel( i18n("Audio language:"), this );
-    QString wt_language( i18n("Select the language and the surround mode. Typically there are \"MPEG2 Stereo\", \"AC3 2ch and 6ch\" and \"DTS 6ch\". \
-You can probably choose AC3 6ch, AC3 2ch and MPEG2 Stereo in descending order, depending on your language (It doesn't matter if DTS works or not, you can still try it).") );
+    QString wt_language( i18n("Select the language and the surround mode. Typically there are "
+			      "\"MPEG2 Stereo\", \"AC3 2ch and 6ch\" and \"DTS 6ch\". "
+			      "You can probably choose AC3 6ch, AC3 2ch and MPEG2 Stereo in descending "
+			      "order, depending on your language (It doesn't matter if DTS works or not, "
+			      "you can still try it).") );
     QWhatsThis::add( language, wt_language);
     QLabel *deinterlace = new QLabel( i18n("Deinterlace mode:"), this );
-    QString wt_deinterlace( i18n("Select a deinterlace mode if you have interlaced material.<ul><li><strong>Fast</strong> is the standard deinterlace mode</li>\
-<li><strong>Encoder Based</strong> uses the internal deinterlace of the codec (be aware that some codecs may have no deinterlacer)</li>\
-<li><strong>Zoom to Full Frame</strong> no matter how it works but it is rather good and slow. Use this if <strong>Fast</strong> still produces interlace \
-stripes or color artifacts (red shadow, etc.)<li>\
-<li><strong>Drop Field/Half Height</strong> uses only one field and scales it.</li>\
-<li><strong>SmartDeinterlace</strong> provides a smart, motion-based deinterlacing capability. Ported from VirtualDub by Tilmann Bitterberg. Operates in RGB space only.\
-Tilmann did some testing of deinterlace filters available in transcode. You can view and read the result on tibit.org/video/. </li>\
-<li><strong>YUVDeinterlaceMMX</strong> is a deinterlace for YUV mode by Thomas Oestreich.</li></ul>"));
+    QString wt_deinterlace( i18n("Select a deinterlace mode if you have interlaced material."
+				 "<ul><li><strong>Fast</strong> is the standard deinterlace mode</li>"
+				 "<li><strong>Encoder Based</strong> uses the internal deinterlace of the "
+				 "codec (be aware that some codecs may have no deinterlacer)</li>"
+				 "<li><strong>Zoom to Full Frame</strong> no matter how it works but "
+				 "it is rather good and slow. Use this if <strong>Fast</strong> still "
+				 "produces interlace stripes or color artifacts (red shadow, etc.)<li>"
+				 "<li><strong>Drop Field/Half Height</strong> uses only one field and "
+				 "scales it.</li>"
+				 "<li><strong>SmartDeinterlace</strong> provides a smart, motion-based "
+				 "deinterlacing capability. Ported from VirtualDub by Tilmann Bitterberg. "
+				 "Operates in RGB space only. Tilmann did some testing of deinterlace filters "
+				 "available in transcode. You can view and read the result on tibit.org/video/. </li>"
+				 "<li><strong>YUVDeinterlaceMMX</strong> is a deinterlace for YUV mode by "
+				 "Thomas Oestreich.</li></ul>"));
     QWhatsThis::add( deinterlace, wt_deinterlace);
     QGroupBox *groupCrispness = new QGroupBox( this );
     groupCrispness->setColumnLayout(0, Qt::Horizontal );
@@ -94,11 +109,14 @@ Tilmann did some testing of deinterlace filters available in transcode. You can 
     QWhatsThis::add( groupCrispness, wt_crispness);
 
     m_checkResample = new QCheckBox( i18n( "Resample to 44.1 kHz" ), this );
-    QWhatsThis::add( m_checkResample, i18n("If this is checked, the audio track will be downsampled from 48 kHz to 44.1 kHz. Useful because some soundcards \
-have problems with a sampling rate of 48 kHz."));
+    QWhatsThis::add( m_checkResample, i18n("If this is checked, the audio track will be downsampled "
+					   "from 48 kHz to 44.1 kHz. Useful because some soundcards "
+					   "have problems with a sampling rate of 48 kHz."));
     m_checkYuv = new QCheckBox( i18n( "Use YUV colorspace." ), this );
-    QWhatsThis::add( m_checkYuv, i18n("If this is checked, the internal codec works with the YUV colorspace instead of using RGB. Useful because DVD \
-also has YUV colorspace and it is twice fast as RGB. You only need RGB colorspace if a filter needs it (filters not yet supported)."));
+    QWhatsThis::add( m_checkYuv, i18n("If this is checked, the internal codec works with the YUV "
+				      "colorspace instead of using RGB. Useful because DVD "
+				      "also has YUV colorspace and it is twice fast as RGB. You only "
+				      "need RGB colorspace if a filter needs it (filters not yet supported)."));
     m_comboDeinterlace = new KComboBox( this );
     m_comboDeinterlace->insertItem(i18n("0 - None"));
     m_comboDeinterlace->insertItem(i18n("1 - Fast"));
