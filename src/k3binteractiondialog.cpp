fc0cd52a2808deaf8f6cc6af80ed4aeffc3ced49
Sebastian Trueg <sebastian@trueg.de>
2005-06-18 18:13:49 +0000

    Keep last used settings when reopening dialog after the job finished.
    svn path=/branches/stable/extragear/multimedia/k3b/; revision=426857

--- src/k3binteractiondialog.cpp
+++ src/k3binteractiondialog.cpp
@@ -156,9 +156,7 @@ K3bInteractionDialog::~K3bInteractionDialog()
 
 void K3bInteractionDialog::show()
 {
-  slotLoadUserDefaults();
   KDialog::show();
-  init();
 }
 
 
@@ -429,8 +427,10 @@ int K3bInteractionDialog::exec( bool returnOnHide )
   setWFlags( WShowModal );
   setResult( 0 );
 
+  slotLoadUserDefaults();
   show();
-  
+  init();
+
   m_inLoop = true;
   QApplication::eventLoop()->enterLoop();
   
