497112ff6d40091d63d4b2f621808c145f8e370e
Markus Brueffer <markus@brueffer.de>
2005-07-12 14:28:57 +0000

    Fix build with gcc 2.95
    svn path=/branches/stable/extragear/multimedia/k3b/; revision=434001

--- src/rip/k3baudioprojectconvertingthread.cpp
+++ src/rip/k3baudioprojectconvertingthread.cpp
@@ -373,7 +373,7 @@ bool K3bAudioProjectConvertingThread::writeCueFile()
 
   // we always use a relative filename here
   QString imageFile = m_tracks[0].second.section( '/', -1 );
-  cueWriter.setImage( imageFile, ( d->fileType.isEmpty() ? "WAVE" : d->fileType ) );
+  cueWriter.setImage( imageFile, ( d->fileType.isEmpty() ? QString("WAVE") : d->fileType ) );
 
   // use the same base name as the image file
   QString cueFile = m_tracks[0].second;
--- src/rip/k3baudioripthread.cpp
+++ src/rip/k3baudioripthread.cpp
@@ -536,7 +536,7 @@ bool K3bAudioRipThread::writeCueFile()
 
   // we always use a relative filename here
   QString imageFile = m_tracks[0].second.section( '/', -1 );
-  cueWriter.setImage( imageFile, ( d->fileType.isEmpty() ? "WAVE" : d->fileType ) );
+  cueWriter.setImage( imageFile, ( d->fileType.isEmpty() ? QString("WAVE") : d->fileType ) );
 
   // use the same base name as the image file
   QString cueFile = m_tracks[0].second;
