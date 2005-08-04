ddd979cdcc59248485a07c38b3fe7c71c95fa0b6
Laurent Montel <montel@kde.org>
2005-08-04 14:54:50 +0000

    Fix crash when reader device is empty (Some guy can backport to HEAD ?)
    svn path=/branches/stable/extragear/multimedia/k3b/; revision=442979

--- src/misc/k3bdvdcopydialog.cpp
+++ src/misc/k3bdvdcopydialog.cpp
@@ -363,7 +363,7 @@ void K3bDvdCopyDialog::slotToggleAll()
   if( m_checkOnlyCreateImage->isChecked() )
     m_checkDeleteImages->setChecked( false );
   
-  m_buttonStart->setEnabled( dev || m_checkOnlyCreateImage->isChecked() );
+  m_buttonStart->setEnabled( (k3bcore->deviceManager()->dvdReader().count() > 0) && (dev || m_checkOnlyCreateImage->isChecked()) );
 }
 
 
