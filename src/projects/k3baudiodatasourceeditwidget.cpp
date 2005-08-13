9a51b9882f79a8d8968cde29463b5946fe1e773c
Sebastian Trueg <sebastian@trueg.de>
2005-08-13 09:00:10 +0000

    The new source editing widget allows to trim the start and end offset of a source.
    svn path=/trunk/extragear/multimedia/k3b/; revision=448835

---
+++ src/projects/k3baudiodatasourceeditwidget.cpp
@@ -0,0 +1,160 @@
+/* 
+ *
+ * $Id: sourceheader 380067 2005-01-19 13:03:46Z trueg $
+ * Copyright (C) 2005 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2005 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "k3baudiodatasourceeditwidget.h"
+#include "k3baudioeditorwidget.h"
+#include <k3bmsfedit.h>
+
+#include <k3baudiodatasource.h>
+
+#include <qlayout.h>
+#include <qlabel.h>
+
+#include <klocale.h>
+#include <kdialog.h>
+
+
+K3bAudioDataSourceEditWidget::K3bAudioDataSourceEditWidget( QWidget* parent, const char* name )
+  : QWidget( parent, name ),
+    m_source(0)
+{
+  m_editor = new K3bAudioEditorWidget( this );
+  m_editStartOffset = new K3bMsfEdit( this );
+  m_editEndOffset = new K3bMsfEdit( this );
+
+  QLabel* startLabel = new QLabel( i18n("Start Offset") + ":", this );
+  QLabel* endLabel = new QLabel( i18n("End Offset") + ":", this );
+  endLabel->setAlignment( Qt::AlignRight );
+
+  QGridLayout* grid = new QGridLayout( this );
+  grid->setMargin( 0 );
+  grid->setSpacing( KDialog::spacingHint() );
+
+  grid->addWidget( startLabel, 0, 0 );
+  grid->addWidget( m_editStartOffset, 1, 0 );
+  grid->addMultiCellWidget( m_editor, 0, 1, 1, 1 );
+  grid->addWidget( endLabel, 0, 2 );
+  grid->addWidget( m_editEndOffset, 1, 2 );
+  grid->setColStretch( 1, 1 );
+
+  // setup connections between the msfedits and the editor
+  connect( m_editor, SIGNAL(rangeChanged(int, const K3b::Msf&, const K3b::Msf&)),
+	   this, SLOT(slotRangeModified(int, const K3b::Msf&, const K3b::Msf&)) );
+
+  connect( m_editStartOffset, SIGNAL(valueChanged(const K3b::Msf&)),
+	   this, SLOT(slotStartOffsetEdited(const K3b::Msf&)) );
+
+  connect( m_editEndOffset, SIGNAL(valueChanged(const K3b::Msf&)),
+	   this, SLOT(slotEndOffsetEdited(const K3b::Msf&)) );
+}
+
+
+K3bAudioDataSourceEditWidget::~K3bAudioDataSourceEditWidget()
+{
+}
+
+
+K3b::Msf K3bAudioDataSourceEditWidget::startOffset() const
+{
+  return m_editor->rangeStart( m_rangeId );
+}
+
+
+K3b::Msf K3bAudioDataSourceEditWidget::endOffset() const
+{
+  return m_editor->rangeEnd( m_rangeId );
+}
+
+
+void K3bAudioDataSourceEditWidget::loadSource( K3bAudioDataSource* source )
+{
+  m_source = source;
+
+  // remove old range
+  m_editor->removeRange( m_rangeId );
+
+  // and add the proper new range
+  // the source's end offset points after the last sector while 
+  // the editor widget returns the last used sector
+  m_editor->setLength( source->originalLength() );
+  m_rangeId = m_editor->addRange( source->startOffset(),
+				  source->endOffset() == 0 
+				  ? source->originalLength()-1
+				  : source->endOffset()-1,
+				  false, 
+				  false,
+				  colorGroup().highlight() );
+
+  m_editStartOffset->setMaxValue( source->originalLength().lba() );
+  m_editEndOffset->setMaxValue( source->originalLength().lba() );
+
+  m_editStartOffset->setMsfValue( startOffset() );
+  m_editEndOffset->setMsfValue( endOffset() );
+}
+
+
+void K3bAudioDataSourceEditWidget::saveSource()
+{
+  if( m_source ) {
+    m_source->setStartOffset( startOffset() );
+    // the source's end offset points after the last sector while 
+    // the editor widget returns the last used sector
+    m_source->setEndOffset( endOffset()+1 );
+  }
+}
+
+
+void K3bAudioDataSourceEditWidget::setStartOffset( const K3b::Msf& msf )
+{
+  if( m_source ) {
+    m_editor->modifyRange( m_rangeId,
+			   msf,
+			   endOffset() );
+  }
+}
+
+
+void K3bAudioDataSourceEditWidget::setEndOffset( const K3b::Msf& msf )
+{
+  if( m_source ) {
+    m_editor->modifyRange( m_rangeId,
+			   startOffset(),
+			   msf );
+  }
+}
+
+
+void K3bAudioDataSourceEditWidget::slotRangeModified( int, const K3b::Msf& start, const K3b::Msf& end )
+{
+  m_editStartOffset->setMsfValue( start );
+  m_editEndOffset->setMsfValue( end );
+}
+
+void K3bAudioDataSourceEditWidget::slotStartOffsetEdited( const K3b::Msf& msf )
+{
+  if( m_source ) {
+    m_editor->modifyRange( m_rangeId, msf, endOffset() );
+  }
+}
+
+
+void K3bAudioDataSourceEditWidget::slotEndOffsetEdited( const K3b::Msf& msf )
+{
+  if( m_source ) {
+    m_editor->modifyRange( m_rangeId, startOffset(), msf );
+  }
+}
+
+#include "k3baudiodatasourceeditwidget.moc"
---
+++ src/projects/k3baudiodatasourceeditwidget.h
@@ -0,0 +1,70 @@
+/* 
+ *
+ * $Id: sourceheader 380067 2005-01-19 13:03:46Z trueg $
+ * Copyright (C) 2005 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2005 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef _K3B_AUDIODATASOURCE_EDITWIDGET_H_
+#define _K3B_AUDIODATASOURCE_EDITWIDGET_H_
+
+#include <qwidget.h>
+#include <k3bmsf.h>
+
+class K3bAudioDataSource;
+class K3bAudioEditorWidget;
+class K3bMsfEdit;
+
+/**
+ * Widget to modify the start and end offset of a source or simply change
+ * the length of a silence source.
+ */
+class K3bAudioDataSourceEditWidget : public QWidget
+{
+  Q_OBJECT
+
+ public:
+  K3bAudioDataSourceEditWidget( QWidget* parent = 0, const char* name = 0 );
+  ~K3bAudioDataSourceEditWidget();
+
+  K3b::Msf startOffset() const;
+
+  /**
+   * Highest value (mening to use all the data up to the end of the source)
+   * is source::originalLength(). 
+   *
+   * Be aware that this differs from K3bAudioDataSource::endOffset() which 
+   * points after the last used sector for internal reasons.
+   */
+  K3b::Msf endOffset() const;
+
+ public slots:
+  void loadSource( K3bAudioDataSource* );
+  void saveSource();
+
+  void setStartOffset( const K3b::Msf& );
+  void setEndOffset( const K3b::Msf& );
+
+ private slots:
+  void slotRangeModified( int, const K3b::Msf&, const K3b::Msf& );
+  void slotStartOffsetEdited( const K3b::Msf& );
+  void slotEndOffsetEdited( const K3b::Msf& );
+
+ private:
+  K3bAudioDataSource* m_source;
+  int m_rangeId;
+
+  K3bAudioEditorWidget* m_editor;
+  K3bMsfEdit* m_editStartOffset;
+  K3bMsfEdit* m_editEndOffset;
+};
+
+#endif
