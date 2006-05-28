7018e499007a5e4a4ccc161ef62eafd69f9f6a49
Sebastian Trueg <sebastian@trueg.de>
2006-05-28 14:31:03 +0000

    Load VideoDVD default seperately ignoring multisession settings to ensure it is always NONE. This should finally fix VideoDVD creation for good.
    svn path=/trunk/extragear/multimedia/k3b/; revision=545856

--- src/k3bprojectmanager.cpp
+++ src/k3bprojectmanager.cpp
@@ -329,8 +329,7 @@ void K3bProjectManager::loadDefaults( K3bDoc* doc )
   }
 
   case K3bDoc::DATA:
-  case K3bDoc::DVD:
-  case K3bDoc::VIDEODVD: {
+  case K3bDoc::DVD: {
     K3bDataDoc* dataDoc = static_cast<K3bDataDoc*>(doc);
 
     dataDoc->setIsoOptions( K3bIsoOptions::load( c ) );
@@ -357,6 +356,13 @@ void K3bProjectManager::loadDefaults( K3bDoc* doc )
     else
       dataDoc->setMultiSessionMode( K3bDataDoc::AUTO );
 
+    break;
+  }
+
+  case K3bDoc::VIDEODVD: {
+    // the only defaults we need here are the volume id and stuff
+    K3bDataDoc* dataDoc = static_cast<K3bDataDoc*>(doc);
+    dataDoc->setIsoOptions( K3bIsoOptions::load( c ) );
     break;
   }
 
