af33fbdb9872d7349a35eb9d2de3c84472510574
Sebastian Trueg <sebastian@trueg.de>
2006-06-27 13:06:21 +0000

    Added detailed warning message in case the user adds boot images that have not the proper size to be a floppy image.
    svn path=/trunk/extragear/multimedia/k3b/; revision=555435

--- src/projects/k3bbootimageview.cpp
+++ src/projects/k3bbootimageview.cpp
@@ -23,6 +23,7 @@
 #include <klistview.h>
 #include <kfiledialog.h>
 #include <kdebug.h>
+#include <kmessagebox.h>
 
 #include <qpushbutton.h>
 #include <qstring.h>
@@ -60,6 +61,8 @@ public:
 	return i18n("None");
     }
     else if( col == 1 )
+      return QString( "%1 KB" ).arg( m_image->size()/1024 );
+    else if( col == 2 )
       return m_image->localPath();
     else
       return QString::null;
@@ -125,7 +128,38 @@ void K3bBootImageView::slotNewBootImage()
 {
   QString file = KFileDialog::getOpenFileName( QString::null, QString::null, this, i18n("Please Choose Boot Image") );
   if( !file.isEmpty() ) {
-    m_doc->createBootItem( file );
+    KIO::filesize_t fsize = K3b::filesize( file );
+    int boottype = K3bBootItem::FLOPPY;
+    if( fsize != 1200*1024 &&
+	fsize != 1440*1024 &&
+	fsize != 2880*1024 ) {
+      switch( KMessageBox::warningYesNoCancel( this, 
+					       i18n("<p>The file you selected is not a floppy image (floppy images are "
+						    "of size 1200 KB, 1440 KB, or 2880 KB). You may still use boot images "
+						    "of other sizes by emulating a harddisk or disabling emulation completely. "
+						    "<p>If you are not familiar with terms like 'harddisk emulation' you most "
+						    "likely want to use a floppy image here. Floppy images can be created by "
+						    "directly extracting them from a real floppy disk:"
+						    "<pre>dd if=/dev/floppy of=/tmp/floppy.img</pre>"
+						    "or by using one of the many boot floppy generators that can be found on "
+						    "<a href=\"http://www.google.com/search?q=linux+boot+floppy&ie=UTF-8&oe=UTF-8\">the internet</a>."),
+					       i18n("No Floppy image selected"),
+					       i18n("Use harddisk emulation"),
+					       i18n("Use no emulation"),
+					       QString::null,
+					       KMessageBox::AllowLink ) ) {
+      case KMessageBox::Yes:
+	boottype = K3bBootItem::HARDDISK;
+	break;
+      case KMessageBox::No:
+	boottype = K3bBootItem::NONE;
+	break;
+      default:
+	return;
+      }
+    }
+
+    m_doc->createBootItem( file )->setImageType( boottype );
     updateBootImages();
   }
 }
@@ -182,6 +216,12 @@ void K3bBootImageView::loadBootItemSettings( K3bBootItem* item )
       m_radioHarddisk->setChecked(true);      
     else
       m_radioNoEmulation->setChecked(true);
+
+    // force floppy size
+    KIO::filesize_t fsize = K3b::filesize( item->localPath() );
+    m_radioFloppy->setDisabled( fsize != 1200*1024 &&
+				fsize != 1440*1024 &&
+				fsize != 2880*1024 );	
   }
   else {
     m_groupOptions->setEnabled(false);
