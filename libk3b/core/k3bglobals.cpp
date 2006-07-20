74df277cf22d1f87f4c9e25b6a4437bed7a14387
Sebastian Trueg <sebastian@trueg.de>
2006-07-20 09:36:44 +0000

    more freebsd compile fixes
    svn path=/branches/stable/extragear/multimedia/k3b/; revision=564458

--- libk3b/core/k3bglobals.cpp
+++ libk3b/core/k3bglobals.cpp
@@ -41,16 +41,16 @@
 
 #include <config.h>
 #if defined(__FreeBSD__) || defined(__NetBSD__)
-#include <sys/param.h>
-#include <sys/mount.h>
-#else
-#  ifdef HAVE_SYS_STATVFS_H
-#    include <sys/statvfs.h>
-#  endif
-#  ifdef HAVE_SYS_STATFS_H
-#    include <sys/vfs.h>
-#  endif
+#  include <sys/param.h>
+#  include <sys/mount.h>
 #endif
+#ifdef HAVE_SYS_STATVFS_H
+#  include <sys/statvfs.h>
+#endif
+#ifdef HAVE_SYS_STATFS_H
+#  include <sys/vfs.h>
+#endif
+
 
 
 
