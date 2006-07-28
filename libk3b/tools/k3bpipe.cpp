6d46e6cb79f877dc624ed8fb3dcc1418d2f9f4f9
Sebastian Trueg <sebastian@trueg.de>
2006-07-28 12:55:24 +0000

    New little helper class which creates a pair of sockets.
    svn path=/trunk/extragear/multimedia/k3b/; revision=567261

---
+++ libk3b/tools/k3bpipe.cpp
@@ -0,0 +1,79 @@
+/* 
+ *
+ * $Id: sourceheader 511311 2006-02-19 14:51:05Z trueg $
+ * Copyright (C) 2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "k3bpipe.h"
+
+#include <kdebug.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+
+K3bPipe::K3bPipe()
+{
+  m_fd[0] = m_fd[1] = -1;
+}
+
+
+K3bPipe::~K3bPipe()
+{
+  close();
+}
+
+
+bool K3bPipe::open()
+{
+  close();
+
+  if( ::socketpair( AF_UNIX, SOCK_STREAM, 0, m_fd ) == 0 ) {
+    fcntl( m_fd[0], F_SETFD, FD_CLOEXEC );
+    fcntl( m_fd[1], F_SETFD, FD_CLOEXEC );
+    return true;
+  }
+  else {
+    kdDebug() << "(K3bPipe) failed to setup socket pair." << endl;
+    return false;
+  }
+}
+
+
+void K3bPipe::closeIn()
+{
+  if( m_fd[1] != -1 ) {
+    ::close( m_fd[1] );
+    m_fd[1] = -1;
+  }
+}
+
+
+void K3bPipe::closeOut()
+{
+  if( m_fd[0] != -1 ) {
+    ::close( m_fd[0] );
+    m_fd[0] = -1;
+  }
+}
+
+
+void K3bPipe::close()
+{
+  closeIn();
+  closeOut();
+}
---
+++ libk3b/tools/k3bpipe.h
@@ -0,0 +1,58 @@
+/* 
+ *
+ * $Id: sourceheader 511311 2006-02-19 14:51:05Z trueg $
+ * Copyright (C) 2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef _K3B_PIPE_H_
+#define _K3B_PIPE_H_
+
+/**
+ * The K3bPipe class represents a file descriptor pair
+ * which can for example be used to connect two processes
+ */
+class K3bPipe
+{
+ public:
+  /**
+   * Creates a closed pipe object
+   */
+  K3bPipe();
+
+  /**
+   * The destructor takes care of closing the pipe
+   */
+  ~K3bPipe();
+
+  /**
+   * Open the pipe. This creates a file descriptor pair
+   * which can be accessed using the in() and out()
+   * methods.
+   */
+  bool open();
+
+  /**
+   * Calls both closeIn() and closeOut()
+   */
+  void close();
+
+  void closeIn();
+  void closeOut();
+
+  int in() const { return m_fd[1]; }
+  int out() const { return m_fd[0]; }
+
+ private:
+  int m_fd[2];
+};
+
+#endif
