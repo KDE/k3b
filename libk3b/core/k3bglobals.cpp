b969b435049a64b749c7ed7713b61feb17c894b8
Sebastian Trueg <sebastian@trueg.de>
2006-08-24 09:35:55 +0000

    Removed the HAVE_STATFS #ifdef as it does not work on some systems and I don't understand its use anyway. I will readd it once it works properly. I think it was from some FreeBSD patch.
    svn path=/trunk/extragear/multimedia/k3b/; revision=576493

--- libk3b/core/k3bglobals.cpp
+++ libk3b/core/k3bglobals.cpp
@@ -261,19 +261,9 @@ QString K3b::systemName()
 
 bool K3b::kbFreeOnFs( const QString& path, unsigned long& size, unsigned long& avail )
 {
-#ifdef HAVE_STATVFS
   struct statvfs fs;
   if( ::statvfs( QFile::encodeName(path), &fs ) == 0 ) {
     unsigned long kBfak = fs.f_frsize/1024;
-#else
-#  ifndef HAVE_STATFS
-#    error "No statfs, no statvfs? Help!"
-#  endif
-  struct statfs fs;
-
-  if( ::statfs( QFile::encodeName(path), &fs ) == 0 ) {
-    unsigned long kBfak = fs.f_bsize/1024;
-#endif
 
     size = fs.f_blocks*kBfak;
     avail = fs.f_bavail*kBfak;
