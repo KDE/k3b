ee8af471379430255466441404328240cacc4784
Sebastian Trueg <sebastian@trueg.de>
2006-09-06 10:20:00 +0000

    New K3bToolButton class which behaves a little like the ktoolbartoolbutton. It opens the popupmenu instantly once the mouse it dragged downwards and displays a little arrow.
    svn path=/trunk/extragear/multimedia/k3b/; revision=581388

--- libk3b/tools/k3bpushbutton.h
+++ libk3b/tools/k3bpushbutton.h
@@ -1,10 +1,10 @@
 /* 
  *
  * $Id$
- * Copyright (C) 2004 Sebastian Trueg <trueg@k3b.org>
+ * Copyright (C) 2004-2006 Sebastian Trueg <trueg@k3b.org>
  *
  * This file is part of the K3b project.
- * Copyright (C) 1998-2004 Sebastian Trueg <trueg@k3b.org>
+ * Copyright (C) 1998-2006 Sebastian Trueg <trueg@k3b.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -19,6 +19,7 @@
 
 #include <kpushbutton.h>
 #include "k3b_export.h"
+
 /**
  * A pushbutton with delayed popu pmenu support just like the KToolBarButton
  */
