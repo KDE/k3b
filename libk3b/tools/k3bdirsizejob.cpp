a80e1d3784c571446fe5465f2516a9b83da53a4b
Sebastian Trueg <sebastian@trueg.de>
2006-11-17 16:43:56 +0000

    KDirSize replacement with a follow symlinks setting and a restriction to local files.
    svn path=/trunk/extragear/multimedia/k3b/; revision=605648

---
+++ libk3b/tools/k3bdirsizejob.cpp
@@ -0,0 +1,183 @@
+/* 
+ *
+ * $Id: sourceheader 511311 2006-02-19 14:51:05Z trueg $
+ * Copyright (C) 2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "k3bdirsizejob.h"
+
+#include <k3bthread.h>
+#include <k3bthreadjob.h>
+#include <k3bsimplejobhandler.h>
+#include <k3bglobals.h>
+
+#include <kdebug.h>
+#include <kglobal.h>
+
+#include <qfileinfo.h>
+#include <qdir.h>
+
+
+class K3bDirSizeJob::WorkThread : public K3bThread
+{
+public:
+  WorkThread()
+    : K3bThread(),
+      followSymlinks(false),
+      totalSize(0),
+      totalFiles(0),
+      totalDirs(0),
+      totalSymlinks(0) {
+  }
+
+  void init() {
+    m_canceled = false;
+
+    totalSize = 0;
+    totalFiles = 0;
+    totalDirs = 0;
+    totalSymlinks = 0;
+  }
+
+  void run() {
+    emitStarted();
+
+    QStringList l;
+    for( KURL::List::const_iterator it = urls.begin();
+	 it != urls.end(); ++it ) {
+      const KURL& url = *it;
+
+      if( !url.isLocalFile() ) {
+	kdDebug() << "(K3bDirSizeJob) no remote support." << endl;
+	emitFinished( false );
+	return;
+      }
+
+      l.append( url.path() );
+    }
+
+    emitFinished( countFiles( l, QString() ) );
+  }
+  
+  bool countDir( const QString& dir ) {
+    const QString& dot = KGlobal::staticQString( "." );
+    const QString& dotdot = KGlobal::staticQString( ".." );
+    QStringList l = QDir(dir).entryList( QDir::All|QDir::Hidden|QDir::System );
+    l.remove( dot );
+    l.remove( dotdot );
+
+    return countFiles( l, dir );
+  }
+
+
+  bool countFiles( const QStringList& l, const QString& dir ) {
+    for( QStringList::const_iterator it = l.begin();
+	 it != l.end(); ++it ) {
+
+      if( m_canceled )
+	return false;
+
+      k3b_struct_stat s;
+      if( k3b_lstat( QFile::encodeName( dir + *it ), &s ) )
+	return false;	
+
+      if( S_ISLNK( s.st_mode ) ) {
+	++totalSymlinks;
+	if( followSymlinks ) {
+	  if( k3b_stat( QFile::encodeName( dir + *it ), &s ) )
+	    return false;	
+	}
+      }
+
+      if( S_ISDIR( s.st_mode ) ) {
+	++totalDirs;
+	if( !countDir( dir + *it + '/' ) )
+	  return false;
+      }
+      else if( !S_ISLNK( s.st_mode ) ) {
+	++totalFiles;
+	totalSize += (KIO::filesize_t)s.st_size;
+      }
+    }
+
+    return true;
+  }
+
+  void cancel() {
+    m_canceled = true;
+    emitCanceled();
+  }
+
+  KURL::List urls;
+  bool followSymlinks;
+
+  KIO::filesize_t totalSize;
+  KIO::filesize_t totalFiles;
+  KIO::filesize_t totalDirs;
+  KIO::filesize_t totalSymlinks;
+
+private:
+  bool m_canceled;
+};
+
+
+K3bDirSizeJob::K3bDirSizeJob( QObject* parent )
+  : K3bThreadJob( new K3bSimpleJobHandler(), parent )
+{
+  d = new WorkThread;
+  setThread( d );
+}
+
+
+K3bDirSizeJob::~K3bDirSizeJob()
+{
+  delete d;
+  delete jobHandler();
+}
+
+
+KIO::filesize_t K3bDirSizeJob::totalSize() const
+{
+  return d->totalSize;
+}
+
+
+KIO::filesize_t K3bDirSizeJob::totalFiles() const
+{
+  return d->totalFiles;
+}
+
+
+KIO::filesize_t K3bDirSizeJob::totalDirs() const
+{
+  return d->totalDirs;
+}
+
+
+KIO::filesize_t K3bDirSizeJob::totalSymlinks() const
+{
+  return d->totalSymlinks;
+}
+
+
+void K3bDirSizeJob::setUrls( const KURL::List& urls )
+{
+  d->urls = urls;
+}
+
+
+void K3bDirSizeJob::setFollowSymlinks( bool b )
+{
+  d->followSymlinks = b;
+}
+
+#include "k3bdirsizejob.moc"
---
+++ libk3b/tools/k3bdirsizejob.h
@@ -0,0 +1,67 @@
+/* 
+ *
+ * $Id: sourceheader 511311 2006-02-19 14:51:05Z trueg $
+ * Copyright (C) 2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2006 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef _K3B_DIR_SIZE_JOB_H_
+#define _K3B_DIR_SIZE_JOB_H_
+
+#include <k3bthreadjob.h>
+#include <kio/global.h>
+
+#include <k3b_export.h>
+
+/**
+ * K3bDirSizeJob is a replacement for KDirSize which allows
+ * a much finer grained control over what is counted and how.
+ * Additionally it uses threading for enhanced speed.
+ *
+ * For now K3bDirSizeJob only works on local urls.
+ */
+class LIBK3B_EXPORT K3bDirSizeJob : public K3bThreadJob
+{
+  Q_OBJECT
+
+ public:
+  K3bDirSizeJob( QObject* parent = 0 );
+  ~K3bDirSizeJob();
+
+  KIO::filesize_t totalSize() const;
+
+  /**
+   * Does also include symlinks to files, devices, and fifos
+   */
+  KIO::filesize_t totalFiles() const;
+
+  /**
+   * Total number of counted dirs. This does also
+   * include the first dirs the job was started with.
+   * Does also include symlinks to dirs.
+   */
+  KIO::filesize_t totalDirs() const;
+
+  /**
+   * Includes symlinks to files and folders
+   */
+  KIO::filesize_t totalSymlinks() const;
+
+ public slots:
+  void setUrls( const KURL::List& urls );
+  void setFollowSymlinks( bool );
+
+ private:
+  class WorkThread;
+  WorkThread* d;
+};
+
+#endif
