1ce370b81d8e86bd30abdcaf520f52f83f1d25ab
Sebastian Trueg <sebastian@trueg.de>
2006-11-28 20:09:49 +0000

    Delete the view item when removing an entry.
    BUG: 137817
    
    svn path=/trunk/extragear/multimedia/k3b/; revision=608888

--- plugins/encoder/external/k3bexternalencoderconfigwidget.cpp
+++ plugins/encoder/external/k3bexternalencoderconfigwidget.cpp
@@ -177,6 +177,7 @@ void K3bExternalEncoderSettingsWidget::slotRemoveCommand()
 {
   if( QListViewItem* item = w->m_viewEncoders->selectedItem() ) {
     d->commands.erase( item );
+    delete item;
   }
 }
 
