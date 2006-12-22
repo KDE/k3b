beebc65ef266508cbb36840a46075502fadca886
Sebastian Trueg <sebastian@trueg.de>
2006-12-22 11:14:44 +0000

    No need to translate the app name and mine.
    CCMAIL: Giovanni Venturi <giovanni@kde-it.org>
    
    svn path=/trunk/extragear/multimedia/k3b/; revision=615700

--- src/main.cpp
+++ src/main.cpp
@@ -74,9 +74,9 @@ static KCmdLineOptions options[] =
 
 int main( int argc, char* argv[] )
 {
-  KAboutData aboutData( "k3b", I18N_NOOP("K3b"),
+  KAboutData aboutData( "k3b", "K3b",
 			LIBK3B_VERSION, description, KAboutData::License_GPL,
-			I18N_NOOP("(c) 1999 - 2006, Sebastian Trüg"), 0, "http://www.k3b.org" );
+			"(c) 1999 - 2006, Sebastian Trüg", 0, "http://www.k3b.org" );
   aboutData.addAuthor("Sebastian Trüg",I18N_NOOP("Maintainer and Lead Developer"), "trueg@k3b.org");
   aboutData.addAuthor("Christian Kvasny",I18N_NOOP("VideoCD Project and VideoCD ripping"), "chris@k3b.org");
   aboutData.addCredit("Klaus-Dieter Krannich", I18N_NOOP("Advanced Cdrdao integration"), "kd@k3b.org" );
