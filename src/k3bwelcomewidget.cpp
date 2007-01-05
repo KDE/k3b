14b8ef307ebe0ca7ccfd169b58e2c7e83ad7966d
Sebastian Trueg <sebastian@trueg.de>
2007-01-05 12:26:26 +0000

    copyright info updated.
    svn path=/trunk/extragear/multimedia/k3b/; revision=620146

--- src/k3bwelcomewidget.cpp
+++ src/k3bwelcomewidget.cpp
@@ -54,7 +54,7 @@ K3bWelcomeWidget::Display::Display( K3bWelcomeWidget* parent )
   fnt.setBold(true);
   fnt.setPointSize( 16 );
   m_header = new QSimpleRichText( i18n("Welcome to K3b - The CD and DVD Kreator"), fnt );
-  m_infoText = new QSimpleRichText( QString::fromUtf8("<qt align=\"center\">K3b %1 (c) 1999 - 2006 Sebastian Trüg")
+  m_infoText = new QSimpleRichText( QString::fromUtf8("<qt align=\"center\">K3b %1 (c) 1999 - 2007 Sebastian Trüg")
 				    .arg(kapp->aboutData()->version()), font() );
 
   // set a large width just to be sure no linebreak occurs
