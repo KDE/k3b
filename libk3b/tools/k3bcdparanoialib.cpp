813deeaa7386c272d6b18ebc1a844d9220759124
Sebastian Trueg <sebastian@trueg.de>
2007-01-09 14:38:25 +0000

    updated copyright info
    svn path=/trunk/extragear/multimedia/k3b/; revision=621693

--- libk3b/tools/k3bcdparanoialib.cpp
+++ libk3b/tools/k3bcdparanoialib.cpp
@@ -1,10 +1,10 @@
 /*
  *
  * $Id$
- * Copyright (C) 2003 Sebastian Trueg <trueg@k3b.org>
+ * Copyright (C) 2003-2007 Sebastian Trueg <trueg@k3b.org>
  *
  * This file is part of the K3b project.
- * Copyright (C) 1998-2004 Sebastian Trueg <trueg@k3b.org>
+ * Copyright (C) 1998-2007 Sebastian Trueg <trueg@k3b.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
