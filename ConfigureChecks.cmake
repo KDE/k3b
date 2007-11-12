54c027793cc5f5971475c9928af11d8642282a42
Laurent Montel <montel@kde.org>
2007-11-12 15:14:34 +0000

    Add option to allow to add extract k3b debug
    svn path=/trunk/extragear/multimedia/k3b/; revision=735762

---
+++ ConfigureChecks.cmake
@@ -0,0 +1,11 @@
+include(CheckIncludeFile)
+include(CheckSymbolExists)
+include(CheckFunctionExists)
+include(CheckLibraryExists)
+include(CheckPrototypeExists)
+include(CheckTypeSize)
+include(CheckStructMember)
+include(CheckCXXSourceCompiles)
+
+macro_bool_to_01(ADD_K3B_DEBUG K3B_DEBUG)
+
