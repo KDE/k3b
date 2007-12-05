00886192f858f882ac9ee591cdc7d13daa6d402a
Laurent Montel <montel@kde.org>
2007-12-05 08:09:15 +0000

    compile with muzicbrainz support
    svn path=/trunk/extragear/multimedia/k3b/; revision=745045

--- src/k3bmusicbrainz.cpp
+++ src/k3bmusicbrainz.cpp
@@ -59,10 +59,10 @@ int K3bMusicBrainz::query( const Q3CString& trm )
 
   if( KProtocolManager::useProxy() ) {
     KUrl proxy = KProtocolManager::proxyFor("http");
-    mb_SetProxy( d->mb, const_cast<char*>(proxy.host().toLatin1()), short(proxy.port()) );
+    mb_SetProxy( d->mb, const_cast<char*>(proxy.host().toLatin1().constData()), short(proxy.port()) );
   }
 
-  char* args[2];
+  const char* args[2];
   args[0] = trm.data();
   args[1] = 0;
 
