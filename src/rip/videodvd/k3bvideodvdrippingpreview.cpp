948fd4ff326fe610c74af54e101f4e62822fd36b
Sebastian Trueg <sebastian@trueg.de>
2009-03-28 21:13:19 +0000

    Found the transcode option - now transcode >= 1.1.0 also produces previews without delay
    svn path=/trunk/extragear/multimedia/k3b/; revision=946207

--- src/rip/videodvd/k3bvideodvdrippingpreview.cpp
+++ src/rip/videodvd/k3bvideodvdrippingpreview.cpp
@@ -79,14 +79,13 @@ void K3b::VideoDVDRippingPreview::generatePreview( const K3b::VideoDVD::VideoDVD
         *m_process << "--log_no_color";
     *m_process << "-i" << dvd.device()->blockDeviceName();
     *m_process << "-T" << QString("%1,%2").arg(title).arg(chapter);
-    *m_process << "-x" << "dvd,null";
-    if ( bin->version < Version( 1, 1, 0 ) )
+    if ( bin->version < Version( 1, 1, 0 ) ) {
+        *m_process << "-x" << "dvd,null";
         *m_process << "--dvd_access_delay" << "0";
-    else
-#ifdef __GNUC__
-#warning FIXME: How can one specify the delay module option? I could not find anything in the transcode manpage!
-#endif
-        *m_process << "delay" << "0";
+    }
+    else {
+        *m_process << "-x" << "dvd='delay=0',null";
+    }
     *m_process << "-y" << "ppm,null";
     *m_process << "-c" << QString("%1-%2").arg( frame ).arg( frame+1 );
     *m_process << "-Z" << "x200";
