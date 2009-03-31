46fab6090137263c153bee1219990ce004d7f005
Sebastian Trueg <sebastian@trueg.de>
2009-03-31 13:59:46 +0000

    Ups, FileSystemInfo does use statfs...
    svn path=/trunk/extragear/multimedia/k3b/; revision=947438

--- ConfigureChecks.cmake
+++ ConfigureChecks.cmake
@@ -7,4 +7,7 @@ include(CheckTypeSize)
 include(CheckStructMember)
 include(CheckCXXSourceCompiles)
 
-check_function_exists(stat64    HAVE_STAT64)
+check_function_exists(stat64 HAVE_STAT64)
+
+check_include_files(sys/vfs.h HAVE_SYS_VFS_H)
+check_include_files(sys/statvfs.h HAVE_SYS_STATVFS_H)
