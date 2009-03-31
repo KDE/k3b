f18b8ce94c3f3ecea8d40596256240584e7a2090
Ralf Habacker <ralf.habacker@freenet.de>
2009-03-31 14:28:33 +0000

    indention fix
    svn path=/trunk/extragear/multimedia/k3b/; revision=947453

--- libk3bdevice/k3bscsicommand_win.cpp
+++ libk3bdevice/k3bscsicommand_win.cpp
@@ -32,7 +32,7 @@
 
 class K3b::Device::ScsiCommand::Private
 {
-  public:
+public:
     SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER m_cmd;
     
     SCSI_SENSE_DATA m_senseData;
@@ -41,15 +41,15 @@ class K3b::Device::ScsiCommand::Private
 
 void K3b::Device::ScsiCommand::clear()
 {
-  ::memset( &d->m_cmd, 0, sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER) );
+    ::memset( &d->m_cmd, 0, sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER) );
 }
 
 
 unsigned char& K3b::Device::ScsiCommand::operator[]( size_t i )
 {
-  if( d->m_cmd.spt.CdbLength < i+1 )
-    d->m_cmd.spt.CdbLength = i+1;
-  return d->m_cmd.spt.Cdb[i];
+    if( d->m_cmd.spt.CdbLength < i+1 )
+        d->m_cmd.spt.CdbLength = i+1;
+    return d->m_cmd.spt.Cdb[i];
 }
 
 
@@ -57,72 +57,70 @@ int K3b::Device::ScsiCommand::transport( TransportDirection dir,
                                        void* data,
                                        size_t len )
 {
-  bool needToClose = false;
-  ULONG returned = 0;
-  BOOL status = TRUE;
+    bool needToClose = false;
+    ULONG returned = 0;
+    BOOL status = TRUE;
 
-  BYTE * buf;
+    BYTE * buf;
 
-  if( m_device ) {
-    needToClose = !m_device->isOpen();
-    m_device->open( dir == TR_DIR_WRITE );
-    m_deviceHandle = m_device->handle();
-  }
+    if( m_device ) {
+        needToClose = !m_device->isOpen();
+        m_device->open( dir == TR_DIR_WRITE );
+        m_deviceHandle = m_device->handle();
+    }
 
-  if( m_deviceHandle == INVALID_HANDLE_VALUE )
-  {
-    kDebug() << "(K3bScsiCommand::transport) could not perform Win32 IOCTL on invalid handle value" << endl;
-    return -1;
-  }
+    if( m_deviceHandle == INVALID_HANDLE_VALUE ) {
+        kDebug() << "(K3bScsiCommand::transport) could not perform Win32 IOCTL on invalid handle value" << endl;
+        return -1;
+    }
 
-  if ( dir == TR_DIR_READ )
-    d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_IN;
-  else if ( dir == TR_DIR_WRITE )
-    d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_OUT;
-  else
-    d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_IN;
+    if ( dir == TR_DIR_READ )
+        d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_IN;
+    else if ( dir == TR_DIR_WRITE )
+        d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_OUT;
+    else
+        d->m_cmd.spt.DataIn           = SCSI_IOCTL_DATA_IN;
 
-  buf = new BYTE[len];
+    buf = new BYTE[len];
 
-  d->m_cmd.spt.Length             = sizeof(SCSI_PASS_THROUGH_DIRECT);
-  d->m_cmd.spt.SenseInfoLength    = SENSE_LEN_SPTI;
-  d->m_cmd.spt.DataTransferLength = len;
-  d->m_cmd.spt.TimeOutValue       = 2;
-  d->m_cmd.spt.DataBuffer         = buf;
-  d->m_cmd.spt.SenseInfoOffset    = offsetof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER, ucSenseBuf);
+    d->m_cmd.spt.Length             = sizeof(SCSI_PASS_THROUGH_DIRECT);
+    d->m_cmd.spt.SenseInfoLength    = SENSE_LEN_SPTI;
+    d->m_cmd.spt.DataTransferLength = len;
+    d->m_cmd.spt.TimeOutValue       = 2;
+    d->m_cmd.spt.DataBuffer         = buf;
+    d->m_cmd.spt.SenseInfoOffset    = offsetof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER, ucSenseBuf);
 
-  status = DeviceIoControl( m_deviceHandle,
-                            IOCTL_SCSI_PASS_THROUGH_DIRECT,
-                            &(d->m_cmd), sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER),
-                            &(d->m_cmd), sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER),
-                            &returned, NULL);
+    status = DeviceIoControl( m_deviceHandle,
+                IOCTL_SCSI_PASS_THROUGH_DIRECT,
+                &(d->m_cmd), sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER),
+                &(d->m_cmd), sizeof(SCSI_PASS_THROUGH_DIRECT_WITH_BUFFER),
+                &returned, NULL);
 
-  memcpy( data, buf, len );
-  delete [] buf;
+    memcpy( data, buf, len );
+    delete [] buf;
 
-  if( needToClose )
-    m_device->close();
+    if( needToClose )
+        m_device->close();
 
-  // get the sense data on error
-  if ( !status )
-  {
-    kDebug() << "(K3bScsiCommand::transport) ioctl failed: " << GetLastError() << ", returned=" << returned << endl;
-    ::memcpy( &(d->m_senseData), d->m_cmd.ucSenseBuf, SENSE_LEN_SPTI );
-	
-    debugError( d->m_cmd.spt.Cdb[0],
-                d->m_senseData.SD_Error,
-                d->m_senseData.SD_SenseKey,
-                d->m_senseData.SD_ASC,
-                d->m_senseData.SD_ASCQ );
+    // get the sense data on error
+    if ( !status ) {
+        kDebug() << "(K3bScsiCommand::transport) ioctl failed: " << GetLastError() << ", returned=" << returned << endl;
+        ::memcpy( &(d->m_senseData), d->m_cmd.ucSenseBuf, SENSE_LEN_SPTI );
 
-    int errCode =
-      (d->m_senseData.SD_Error << 24)    & 0xF000 |
-      (d->m_senseData.SD_SenseKey << 16) & 0x0F00 |
-      (d->m_senseData.SD_ASC << 8)       & 0x00F0 |
-      (d->m_senseData.SD_ASCQ)           & 0x000F;
+        debugError( d->m_cmd.spt.Cdb[0],
+            d->m_senseData.SD_Error,
+            d->m_senseData.SD_SenseKey,
+            d->m_senseData.SD_ASC,
+            d->m_senseData.SD_ASCQ );
 
-    return ( errCode != 0 ? errCode : 1 );
-  }
+        int errCode =
+            (d->m_senseData.SD_Error << 24)    & 0xF000 |
+            (d->m_senseData.SD_SenseKey << 16) & 0x0F00 |
+            (d->m_senseData.SD_ASC << 8)       & 0x00F0 |
+            (d->m_senseData.SD_ASCQ)           & 0x000F;
 
-  return 0;
+        return ( errCode != 0 ? errCode : 1 );
+    }
+
+    return 0;
 }
