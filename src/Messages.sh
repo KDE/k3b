11662df926606c561b2e14ad3f60cf93462b38ac
Laurent Montel <montel@kde.org>
2010-01-14 08:28:03 +0000

    Remove rc.cpp too
    svn path=/trunk/extragear/multimedia/k3b/; revision=1074447

--- src/Messages.sh
+++ src/Messages.sh
@@ -3,5 +3,5 @@ $EXTRACTRC `find . -name \*.rc -o -name \*.ui` `find ../plugins -name \*.rc -o -
 $PREPARETIPS > tips.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` `find ../plugins -name \*.cpp -o -name \*.h` -o $podir/k3b.pot
 rm -f tips.cpp
-
+rm -f rc.cpp
 
