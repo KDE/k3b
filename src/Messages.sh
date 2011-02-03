3c8b201689eb68a5c1368eed6496bf474af2024b
Pino Toscano <pino@kde.org>
2011-02-03 10:53:52 +0100

    no tips to extract anymore
    followup of dc947cf2fdba9402dd65eea4486eac6b5e96f79f

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui` `find ../plugins -name \*.rc -o -name \*.ui` >> rc.cpp
-$PREPARETIPS > tips.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` `find ../plugins -name \*.cpp -o -name \*.h` -o $podir/k3b.pot
-rm -f tips.cpp
 rm -f rc.cpp
 
