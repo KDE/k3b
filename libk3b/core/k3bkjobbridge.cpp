a58fde92de373519a6ebc159a2492bac32607765
Michal Malek <michalm@jabster.pl>
2011-04-26 08:04:49 +0200

    Added KJobBridge class which works as KJob interface for a K3b::Job

---
+++ libk3b/core/k3bkjobbridge.cpp
@@ -0,0 +1,112 @@
+/*
+ *
+ * Copyright (C) 2011 Michal Malek <michalm@jabster.pl>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2011 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#include "k3bkjobbridge.h"
+#include "k3bjob.h"
+
+namespace K3b
+{
+    
+class KJobBridge::Private
+{
+public:
+    Private( Job& j )
+    :
+        job( j )
+    {
+    }
+    
+    Job& job;
+};
+
+KJobBridge::KJobBridge( Job& job )
+    : d( new Private( job ) )
+{
+    connect( &d->job, SIGNAL(canceled()), this, SLOT(slotCanceled()) );
+    connect( &d->job, SIGNAL(finished(bool)), this, SLOT(slotFinished(bool)) );
+    connect( &d->job, SIGNAL(infoMessage(QString,int)), this, SLOT(slotInfoMessage(QString,int)) );
+    connect( &d->job, SIGNAL(percent(int)), this, SLOT(slotPercent(int)) );
+    connect( &d->job, SIGNAL(processedSize(int,int)), this, SLOT(slotProcessedSize(int,int)) );
+    connect( &d->job, SIGNAL(newTask(QString)), this, SLOT(slotNewTask(QString)) );
+    
+    setCapabilities( Killable );
+}
+
+
+KJobBridge::~KJobBridge()
+{
+}
+
+
+void KJobBridge::start()
+{
+    d->job.start();
+}
+
+
+bool KJobBridge::doKill()
+{
+    d->job.cancel();
+    return true;
+}
+
+
+void KJobBridge::slotCanceled()
+{
+    kill();
+}
+
+
+void KJobBridge::slotFinished( bool success )
+{
+    if( success )
+        setError( NoError );
+    else if( d->job.hasBeenCanceled() )
+        setError( KilledJobError );
+    else
+        setError( UserDefinedError );
+    
+    emitResult();
+}
+
+
+void KJobBridge::slotInfoMessage( const QString& message, int type )
+{
+    if( type == Job::MessageError )
+        setErrorText( message );
+    else if( type == Job::MessageWarning )
+        emit warning( this, message );
+}
+
+
+void KJobBridge::slotPercent( int progress )
+{
+    setPercent( progress );
+}
+
+
+void KJobBridge::slotProcessedSize( int processed, int size )
+{
+    setTotalAmount( Bytes, static_cast<qulonglong>( size ) * 1024ULL * 1024ULL );
+    setProcessedAmount( Bytes, static_cast<qulonglong>( processed ) * 1024ULL * 1024ULL );
+}
+
+
+void KJobBridge::slotNewTask( const QString& task )
+{
+    emit description( this, task );
+    emit infoMessage( this, task );
+}
+
+} // namespace K3b
---
+++ libk3b/core/k3bkjobbridge.h
@@ -0,0 +1,56 @@
+/*
+ *
+ * Copyright (C) 2011 Michal Malek <michalm@jabster.pl>
+ *
+ * This file is part of the K3b project.
+ * Copyright (C) 1998-2011 Sebastian Trueg <trueg@k3b.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
+
+#ifndef K3B_KJOB_BRIDGE_H
+#define K3B_KJOB_BRIDGE_H
+
+#include "k3b_export.h"
+
+#include <KJob>
+#include <QtCore/QScopedPointer>
+
+namespace K3b {
+    
+class Job;
+
+class LIBK3B_EXPORT KJobBridge : public KJob
+{
+    Q_OBJECT
+    Q_DISABLE_COPY( KJobBridge )
+
+public:
+    explicit KJobBridge( Job& job );
+    virtual ~KJobBridge();
+    
+    virtual void start();
+    
+protected:
+    virtual bool doKill();
+    
+private Q_SLOTS:
+    void slotCanceled();
+    void slotFinished( bool success );
+    void slotInfoMessage( const QString& message, int type );
+    void slotPercent( int progress );
+    void slotProcessedSize( int processed, int size );
+    void slotNewTask( const QString& task );
+    
+private:
+    class Private;
+    QScopedPointer<Private> d;
+};
+
+} // namespace K3b
+
+#endif // K3B_KJOB_BRIDGE_H
