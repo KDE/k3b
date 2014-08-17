e181d080c3fa98c83120d1562e374e520ea91cd2
Michał Małek <michalm@jabster.pl>
2014-08-17 12:50:14 +0200

    Enabled searching for all libraries

--- cmake/modules/FindSamplerate.cmake
+++ cmake/modules/FindSamplerate.cmake
@@ -11,8 +11,8 @@
 # Copyright (c) 2007, Laurent Montel, <montel@kde.org>
 
 if(SAMPLERATE_INCLUDE_DIR AND SAMPLERATE_LIBRARIES)
-	# in cache already
-	set(Samplerate_FIND_QUIETLY TRUE)
+    # in cache already
+    set(Samplerate_FIND_QUIETLY TRUE)
 endif()
 
 find_path(SAMPLERATE_INCLUDE_DIR NAMES samplerate.h)
@@ -24,8 +24,8 @@ find_package_handle_standard_args(Samplerate DEFAULT_MSG SAMPLERATE_INCLUDE_DIR 
 
 add_library(samplerate SHARED IMPORTED)
 set_target_properties(samplerate PROPERTIES
-	INTERFACE_INCLUDE_DIRECTORIES "${SAMPLERATE_INCLUDE_DIR}"
-	IMPORTED_LOCATION "${SAMPLERATE_LIBRARIES}"
+    INTERFACE_INCLUDE_DIRECTORIES "${SAMPLERATE_INCLUDE_DIR}"
+    IMPORTED_LOCATION "${SAMPLERATE_LIBRARIES}"
 )
 
 # show the SAMPLERATE_INCLUDE_DIR and SAMPLERATE_LIBRARIES variables only in the advanced view
