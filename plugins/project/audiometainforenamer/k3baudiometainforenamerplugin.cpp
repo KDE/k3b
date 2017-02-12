KDE/k3b

commit cf01ff3159dbddb97e0e67246dee16441a64984a

Fix issues detected by clang-analyzer

plugins/project/audiometainforenamer/k3baudiometainforenamerplugin.cpp
@@ -46,6 +46,7 @@
 #include <QtWidgets/QToolTip>
 #include <QtWidgets/QTreeWidget>
 #include <QtWidgets/QVBoxLayout>
+#include <QSharedPointer>
 
 #include <taglib/tag.h>
 #include <taglib/fileref.h>
@@ -149,7 +150,8 @@ K3bAudioMetainfoRenamerPluginWidget::K3bAudioMetainfoRenamerPluginWidget( K3b::D
                                         "<em>%n</em> (Track number), and <em>%t</em> (Title) "
                                         "are supported.") );
 
-    TagLib::FileRef::addFileTypeResolver( new K3bMimeTypeResolver() );
+    QSharedPointer<K3bMimeTypeResolver> typePtr = QSharedPointer<K3bMimeTypeResolver>(new K3bMimeTypeResolver);
+    TagLib::FileRef::addFileTypeResolver(typePtr.data());
 }
 
 

libk3b/projects/audiocd/k3baudiodoc.cpp
@@ -721,11 +721,13 @@ bool K3b::AudioDoc::loadDocumentData( QDomElement* root )
                                 }
                                 else {
                                     qDebug() << "(K3b::AudioDoc) invalid cdtrack source.";
+                                    delete track;
                                     return false;
                                 }
                             }
                             else {
                                 qDebug() << "(K3b::AudioDoc) unknown source type: " << sourceElem.nodeName();
+                                delete track;
                                 return false;
                             }
                         }

libk3b/projects/movixcd/k3bmovixdocpreparer.cpp
@@ -257,6 +257,8 @@ bool K3b::MovixDocPreparer::addMovixFiles()
     // first of all we create the directories
     d->isolinuxDir = new K3b::DirItem( "isolinux" );
     d->movixDir = new K3b::DirItem( "movix" );
+    if (d->doc == Q_NULLPTR)
+        return false;
     d->doc->root()->addDataItem( d->isolinuxDir );
     d->doc->root()->addDataItem( d->movixDir );
     K3b::DirItem* kernelDir = d->doc->addEmptyDir( "kernel", d->isolinuxDir );
@@ -362,6 +364,8 @@ bool K3b::MovixDocPreparer::addMovixFilesNew()
     // 3. add movixrc and movix.list files
     // 4. set weights for isolinux files
 
+    if (d->doc == Q_NULLPTR)
+        return false;
     // FIXME: use the settings from the doc
     const QStringList files = d->eMovixBin->files( d->doc->keyboardLayout(),
                                              d->doc->subtitleFontset(),

libk3b/projects/movixcd/k3bmovixprogram.cpp
@@ -70,7 +70,7 @@ bool K3b::MovixProgram::scan( const QString& p )
         return false;
     }
 
-    if( bin->version() >= K3b::Version( 0, 9, 0 ) )
+    if( bin && bin->version() >= K3b::Version( 0, 9, 0 ) )
         return scanNewEMovix( bin, path );
     else
         return scanOldEMovix( bin, path );
@@ -106,6 +106,8 @@ bool K3b::MovixProgram::scanOldEMovix( K3b::MovixBin* bin, const QString& path )
     //
     // first check if all necessary directories are present
     //
+    if (bin == Q_NULLPTR)
+        return false;
     QDir dir( bin->movixDataDir() );
     QStringList subdirs = dir.entryList( QDir::Dirs );
     if( !subdirs.contains( "boot-messages" ) ) {

libk3b/projects/videocd/mpeginfo/k3bmpeginfo.cpp
@@ -257,7 +257,9 @@ llong K3b::MpegInfo::MpegParsePacket ( llong offset )
         offset += 4;
         size = GetSize( offset );
         offset += 2;
-        // qDebug() << QString( "offset = %1, size = %2" ).arg( offset ).arg( size );
+#ifdef K3B_DEBUG
+        qDebug() << QString( "offset = %1, size = %2" ).arg( offset ).arg( size );
+#endif
 
         switch ( mark ) {
         case MPEG_SYSTEM_HEADER_CODE:
@@ -760,7 +762,7 @@ void K3b::MpegInfo::ParseVideo ( llong offset, byte marker )
 
     mpeg_info->video[ v_idx ].bitrate = 400 * brate;
 
-    byte mark;
+    byte mark = -1;
     while ( true ) {
         offset = FindNextMarker( offset, &mark );
         if ( mark == MPEG_GOP_CODE )

libk3bdevice/k3bdevice_mmc.cpp
@@ -202,9 +202,7 @@ bool K3b::Device::Device::getPerformance( UByteArray& data,
     //        return types. "Defect Status Data" for example might return way more data.
     // FIXME: Since we only use getPerformance for writing speeds and without a proper length
     //        those do not make sense it is better to fail here anyway.
-    if( (dataLen-8) % descLen ||
-        dataLen <= 8 ||
-        dataLen > 2048 ) {
+    if( descLen == 0 || (dataLen-8) % descLen || dataLen <= 8 || dataLen > 2048 ) {
         qDebug() << "(K3b::Device::Device) " << blockDeviceName()
                  << ": GET PERFORMANCE reports bogus dataLen: " << dataLen << endl;
         return false;
@@ -586,7 +584,7 @@ bool K3b::Device::Device::readTocPmaAtip( UByteArray& data, int format, bool tim
     // with these buggy drives.
     // We cannot use this as default since many firmwares fail with a too high data length.
     //
-    if( (dataLen-4) % descLen || dataLen < 4+descLen ) {
+    if( descLen != 0 && ((dataLen-4) % descLen || dataLen < 4+descLen) ) {
         qDebug() << "(K3b::Device::Device) " << blockDeviceName() << ": READ TOC/PMA/ATIP invalid length returned: " << dataLen;
         dataLen = 0xFFFF;
     }
@@ -605,7 +603,7 @@ bool K3b::Device::Device::readTocPmaAtip( UByteArray& data, int format, bool tim
     cmd[8] = data.size();
     if( cmd.transport( TR_DIR_READ, data.data(), data.size() ) == 0 ) {
         dataLen = qMin( dataLen, from2Byte( data.data() ) + 2u );
-        if( (dataLen-4) % descLen || dataLen < 4+descLen ) {
+        if( descLen == 0 || (dataLen-4) % descLen || dataLen < 4+descLen ) {
             // useless length
             data.clear();
             return false;

src/k3b.cpp
@@ -288,6 +288,7 @@ void K3b::MainWindow::initActions()
     // need to have all actions in the mainwindow's actioncollection anyway (or am I just to stupid to
     // see the correct solution?)
 
+    // clang-analyzer wrongly treat KF5's KStandardAction::open as Unix API Improper use of 'open'
     QAction* actionFileOpen = KStandardAction::open( this, SLOT(slotFileOpen()), actionCollection() );
     actionFileOpen->setToolTip( i18n( "Opens an existing project" ) );
     actionFileOpen->setStatusTip( actionFileOpen->toolTip() );

src/k3bmetaitemmodel.cpp
@@ -180,6 +180,9 @@ namespace {
 
         // all the node mapping is done on the first col, so make sure we use
         // an index on the first col
+        // 
+        // A valid index belongs to a model, and has non-negative row and column numbers
+        // so index.model() is NOT nullptr if index.isValid()
         QModelIndex firstColIndex = index.model()->index(index.row(), 0, index.parent());
         Node* node = findNodeForOriginalIndex( firstColIndex );
         if ( !node ) {

commit b3268756f2681e28f1a85cf479a6bebb670daaff

unbreak K3bAudioMetainfoRenamerPluginWidget
Commit cf01ff3159dbddb97e0e67246dee16441a64984a broke the custom file
resolver for taglib, since the new object is deleted when the scope
ends.

A better solution needs to eventually be found, but for now better leak
one object than crash using stale pointers.

plugins/project/audiometainforenamer/k3baudiometainforenamerplugin.cpp
@@ -46,7 +46,6 @@
 #include <QtWidgets/QToolTip>
 #include <QtWidgets/QTreeWidget>
 #include <QtWidgets/QVBoxLayout>
-#include <QSharedPointer>
 
 #include <taglib/tag.h>
 #include <taglib/fileref.h>
@@ -150,8 +149,7 @@ K3bAudioMetainfoRenamerPluginWidget::K3bAudioMetainfoRenamerPluginWidget( K3b::D
                                         "<em>%n</em> (Track number), and <em>%t</em> (Title) "
                                         "are supported.") );
 
-    QSharedPointer<K3bMimeTypeResolver> typePtr = QSharedPointer<K3bMimeTypeResolver>(new K3bMimeTypeResolver);
-    TagLib::FileRef::addFileTypeResolver(typePtr.data());
+    TagLib::FileRef::addFileTypeResolver( new K3bMimeTypeResolver() );
 }
 
 