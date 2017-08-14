96540735dbcc3189812e6d46cf7d147f006e5654
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-08-14 15:06:33 +0800

    [tests] Remove some testcase might lead UB issue.
    Testcase by Matthias Schwarzott!
    
    CCBUG: 382941

--- tests/k3bdeviceglobalstest.cpp
+++ tests/k3bdeviceglobalstest.cpp
@@ -25,26 +25,22 @@ void DeviceGlobalsTest::testFrom2Byte()
 {
     unsigned char* d = NULL;
     QCOMPARE(K3b::Device::from2Byte(d), (quint16)0);   // Invalid Byte!
-    const unsigned char buf0[] = { '\0' };
-    QCOMPARE(K3b::Device::from2Byte(buf0), (quint16)0);
-    unsigned const char buf1[] = { 0x00, 0x00 };
-    QCOMPARE(K3b::Device::from2Byte(buf1), (quint16)0x0000);
-    unsigned const char buf2[] = { 0x00, 0x70 };
-    QCOMPARE(K3b::Device::from2Byte(buf2), (quint16)0x0070);
-    unsigned const char buf3[] = { 0x05, 0x00 };
-    QCOMPARE(K3b::Device::from2Byte(buf3), (quint16)0x0500);
-    unsigned const char buf4[] = { 0xF0, 0x03 };
-    QCOMPARE(K3b::Device::from2Byte(buf4), (quint16)0xF003);
+    unsigned const char buf0[] = { 0x00, 0x00 };
+    QCOMPARE(K3b::Device::from2Byte(buf0), (quint16)0x0000);
+    unsigned const char buf1[] = { 0x00, 0x70 };
+    QCOMPARE(K3b::Device::from2Byte(buf1), (quint16)0x0070);
+    unsigned const char buf2[] = { 0x05, 0x00 };
+    QCOMPARE(K3b::Device::from2Byte(buf2), (quint16)0x0500);
+    unsigned const char buf3[] = { 0xF0, 0x03 };
+    QCOMPARE(K3b::Device::from2Byte(buf3), (quint16)0xF003);
 }
 
 void DeviceGlobalsTest::testFrom4Byte() 
 {
-    const unsigned char buf0[] = { '\0' };
-    QCOMPARE(K3b::Device::from4Byte(buf0), (quint32)0);
-    unsigned const char buf1[] = { 0x00, 0x00, 0x00, 0x00 };
-    QCOMPARE(K3b::Device::from4Byte(buf1), (quint32)0x00000000);
-    unsigned const char buf2[] = { 0x00, 0x00, 0x00, 0x01 };
-    QCOMPARE(K3b::Device::from4Byte(buf2), (quint32)0x00000001);
-    unsigned const char buf3[] = { 0x12, 0x34, 0x56, 0x78 };
-    QCOMPARE(K3b::Device::from4Byte(buf3), (quint32)0x12345678);
+    unsigned const char buf0[] = { 0x00, 0x00, 0x00, 0x00 };
+    QCOMPARE(K3b::Device::from4Byte(buf0), (quint32)0x00000000);
+    unsigned const char buf1[] = { 0x00, 0x00, 0x00, 0x01 };
+    QCOMPARE(K3b::Device::from4Byte(buf1), (quint32)0x00000001);
+    unsigned const char buf2[] = { 0x12, 0x34, 0x56, 0x78 };
+    QCOMPARE(K3b::Device::from4Byte(buf2), (quint32)0x12345678);
 }
