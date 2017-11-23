d2d1b578acb3a71ca8e6f0c8019c2dc0193819de
Leslie Zhai <lesliezhai@llvm.org.cn>
2017-11-23 11:50:17 +0800

    Update fuzzer testcase for QFile and QString.
    CCBUG: 386983

--- tests/k3bfuzzertest.cpp
+++ tests/k3bfuzzertest.cpp
@@ -1,6 +1,18 @@
-#include <QDebug>
+/*
+ * Copyright (C) 2016 - 2017 Leslie Zhai <lesliezhai@llvm.org.cn>
+ *
+ * This file is part of the K3b project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * See the file "COPYING" for the exact licensing terms.
+ */
 
-#include "tools/libisofs/isofs.h"
+#include <QDebug>
+#include <QFile>
+#include <QString>
 
 extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) 
 {
@@ -11,10 +23,8 @@ extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv)
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) 
 {
-    char *d = (char *)malloc(1024);
-    memset(d, 0, 1024);
-    strncpy(d, "hello", 5);
-    str_nappend(&d, (char *)Data, Size);
-    free(d);
+    // QTBUG-57553
+    qDebug() << "DEBUG:" << __PRETTY_FUNCTION__
+             << QFile::encodeName(QString::fromRawData((const QChar *)Data, Size));
     return 0;
 }
