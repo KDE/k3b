1cfedeb90f37e3772607cdf321c51bb817318c77
Albert Astals Cid <aacid@kde.org>
2019-03-30 19:42:14 +0100

    KAuth needs the return type to be ActionReply
    and not KAuth::ActionReply, since this is done via invokeMethod magic
    and that does string matching

--- src/helper/k3bhelper.h
+++ src/helper/k3bhelper.h
@@ -19,6 +19,8 @@
 #include <KAuthActionReply>
 #include <QObject>
 
+using namespace KAuth;
+
 namespace K3b {
  
 class Helper : public QObject
@@ -36,14 +38,14 @@ public slots:
      * @param programs list of the programs which will have updated permissions. Each element
      *                 of the list is a @see K3b::HelperProgramItem object
      */
-    KAuth::ActionReply updatepermissions( QVariantMap args );
+    ActionReply updatepermissions( QVariantMap args );
 
     /**
      * Adds user to a specified group
      * @param groupName name of the group
      * @param userName name of the user
      */
-    KAuth::ActionReply addtogroup( QVariantMap args );
+    ActionReply addtogroup( QVariantMap args );
 };
 
 } // namespace K3b
