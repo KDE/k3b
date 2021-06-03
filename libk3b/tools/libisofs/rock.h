98051bc9e28ad817e919ed256ac9cce7e2d018f9
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-03 08:27:55 +0200

    GIT_SILENT Revert unrelated changes to files

--- libk3b/tools/libisofs/rock.h
+++ libk3b/tools/libisofs/rock.h
@@ -4,85 +4,85 @@
 #define ROCK_H 1
 
 /* These structs are used by the system-use-sharing protocol, in which the
-    Rock Ridge extensions are embedded.  It is quite possible that other
-    extensions are present on the disk, and this is fine as long as they
-    all use SUSP*/
+   Rock Ridge extensions are embedded.  It is quite possible that other
+   extensions are present on the disk, and this is fine as long as they
+   all use SUSP */
 
 struct SU_SP{
-    unsigned char magic[2];
-    unsigned char skip;
+  unsigned char magic[2];
+  unsigned char skip;
 };
 
 struct SU_CE{
-    char extent[8];
-    char offset[8];
-    char size[8];
+  char extent[8];
+  char offset[8];
+  char size[8];
 };
 
 struct SU_ER{
-    unsigned char len_id;
-    unsigned char len_des;
-    unsigned char len_src;
-    unsigned char ext_ver;
-    char data[1];
+  unsigned char len_id;
+  unsigned char len_des;
+  unsigned char len_src;
+  unsigned char ext_ver;
+  char data[1];
 };
 
 struct RR_RR{
-    char flags[1];
+  char flags[1];
 };
 
 struct RR_PX{
-    char mode[8];
-    char n_links[8];
-    char uid[8];
-    char gid[8];
-    char serno[8];
+  char mode[8];
+  char n_links[8];
+  char uid[8];
+  char gid[8];
+  char serno[8];
 };
 
 struct RR_PN{
-    char dev_high[8];
-    char dev_low[8];
+  char dev_high[8];
+  char dev_low[8];
 };
 
 
 struct SL_component{
-    unsigned char flags;
-    unsigned char len;
-    char text[1];
+  unsigned char flags;
+  unsigned char len;
+  char text[1];
 };
 
 struct RR_SL{
-    unsigned char flags;
-    struct SL_component link;
+  unsigned char flags;
+  struct SL_component link;
 };
 
 struct RR_NM{
-    unsigned char flags;
-    char name[1];
+  unsigned char flags;
+  char name[1];
 };
 
 struct RR_CL{
-    char location[8];
+  char location[8];
 };
 
 struct RR_PL{
-    char location[8];
+  char location[8];
 };
 
 struct stamp{
-    char time[7];
+  char time[7];
 };
 
 struct RR_TF{
-    char flags;
-    struct stamp times[1];  /* Variable number of these beasts*/
+  char flags;
+  struct stamp times[1];  /* Variable number of these beasts */
 };
 
 /* Linux-specific extension for transparent decompression */
 struct RR_ZF{
-    char algorithm[2];
-    char parms[2];
-    char real_size[8];
+  char algorithm[2];
+  char parms[2];
+  char real_size[8];
 };
 
 /* These are the bits and their meanings for flags in the TF structure. */
@@ -96,10 +96,10 @@ struct RR_ZF{
 #define TF_LONG_FORM 128
 
 struct rock_ridge{
-    char signature[2];
-    char len; /* 711*/
-    char version; /* 711*/
-    union{
+  char signature[2];
+  char len; /* 711 */
+  char version; /* 711 */
+  union{
     struct SU_SP SP;
     struct SU_CE CE;
     struct SU_ER ER;
@@ -112,16 +112,16 @@ struct rock_ridge{
     struct RR_PL PL;
     struct RR_TF TF;
     struct RR_ZF ZF;
-    } u;
+  } u;
 };
 
-#define RR_PX 1   /* POSIX attributes*/
-#define RR_PN 2   /* POSIX devices*/
-#define RR_SL 4   /* Symbolic link*/
-#define RR_NM 8   /* Alternate Name*/
-#define RR_CL 16  /* Child link*/
-#define RR_PL 32  /* Parent link*/
-#define RR_RE 64  /* Relocation directory*/
-#define RR_TF 128 /* Timestamps*/
+#define RR_PX 1   /* POSIX attributes */
+#define RR_PN 2   /* POSIX devices */
+#define RR_SL 4   /* Symbolic link */
+#define RR_NM 8   /* Alternate Name */
+#define RR_CL 16  /* Child link */
+#define RR_PL 32  /* Parent link */
+#define RR_RE 64  /* Relocation directory */
+#define RR_TF 128 /* Timestamps */
 
-#endif /* ROCK_H*/
+#endif /* ROCK_H */
