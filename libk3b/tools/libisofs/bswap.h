76dd65fc6bb2de02c5ffd8240a639d2b130395cc
Alexander Lohnau <alexander.lohnau@gmx.de>
2021-06-03 08:27:55 +0200

    GIT_SILENT Revert unrelated changes to files

--- libk3b/tools/libisofs/bswap.h
+++ libk3b/tools/libisofs/bswap.h
@@ -12,9 +12,9 @@
 #ifdef ARCH_X86
 inline static unsigned short ByteSwap16(unsigned short x)
 {
-    __asm("xchgb %b0,%h0"	:
-    "=q" (x)	:
-    "0" (x));
+  __asm("xchgb %b0,%h0"	:
+        "=q" (x)	:
+        "0" (x));
     return x;
 }
 #define bswap_16(x) ByteSwap16(x)
@@ -22,27 +22,27 @@ inline static unsigned short ByteSwap16(unsigned short x)
 inline static unsigned int ByteSwap32(unsigned int x)
 {
 #if defined(__CPU__) && (__CPU__ > 386)
-    __asm("bswap	%0":
-    "=r" (x)     :
+ __asm("bswap	%0":
+      "=r" (x)     :
 #else
-    __asm("xchgb	%b0,%h0\n"
-    "	rorl	$16,%0\n"
-    "	xchgb	%b0,%h0":
-    "=q" (x)		:
+ __asm("xchgb	%b0,%h0\n"
+      "	rorl	$16,%0\n"
+      "	xchgb	%b0,%h0":
+      "=q" (x)		:
 #endif
-    "0" (x));
-    return x;
+      "0" (x));
+  return x;
 }
 #define bswap_32(x) ByteSwap32(x)
 
 inline static unsigned long long int ByteSwap64(unsigned long long int x)
 {
-    register union { __extension__ unsigned long long int __ll;
-    unsigned int __l[2]; } __x;
-    asm("xchgl	%0,%1":
-    "=r"(__x.__l[0]),"=r"(__x.__l[1]):
-    "0"(bswap_32((unsigned long)x)),"1"(bswap_32((unsigned long)(x>>32))));
-    return __x.__ll;
+  register union { __extension__ unsigned long long int __ll;
+          unsigned int __l[2]; } __x;
+  asm("xchgl	%0,%1":
+      "=r"(__x.__l[0]),"=r"(__x.__l[1]):
+      "0"(bswap_32((unsigned long)x)),"1"(bswap_32((unsigned long)(x>>32))));
+  return __x.__ll;
 }
 #define bswap_64(x) ByteSwap64(x)
 
@@ -51,26 +51,26 @@ inline static unsigned long long int ByteSwap64(unsigned long long int x)
 #define bswap_16(x) (((x) & 0x00ff) << 8 | ((x) & 0xff00) >> 8)
 			
 
-/* code from bits/byteswap.h SPDX-FileCopyrightText: 1997, 1998 Free Software Foundation Inc. */
+/* code from bits/byteswap.h (C) 1997, 1998 Free Software Foundation, Inc. */
 #define bswap_32(x) \
-    ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
-    (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
+     ((((x) & 0xff000000) >> 24) | (((x) & 0x00ff0000) >>  8) | \
+      (((x) & 0x0000ff00) <<  8) | (((x) & 0x000000ff) << 24))
 
 #define bswap_64(x) \
-    (__extension__						\
-    ({ union { __extension__ unsigned long long int __ll;	\
-    unsigned int __l[2]; } __w, __r;		\
-    __w.__ll = (x);					\
-    __r.__l[0] = bswap_32 (__w.__l[1]);			\
-    __r.__l[1] = bswap_32 (__w.__l[0]);			\
-    __r.__ll; }))
-#endif	/* !ARCH_X86*/
+     (__extension__						\
+      ({ union { __extension__ unsigned long long int __ll;	\
+                 unsigned int __l[2]; } __w, __r;		\
+         __w.__ll = (x);					\
+         __r.__l[0] = bswap_32 (__w.__l[1]);			\
+         __r.__l[1] = bswap_32 (__w.__l[0]);			\
+         __r.__ll; }))
+#endif	/* !ARCH_X86 */
 
-#endif	/* !HAVE_BYTESWAP_H*/
+#endif	/* !HAVE_BYTESWAP_H */
 
 /*
-    be2me ... BigEndian to MachineEndian
-    le2me ... LittleEndian to MachineEndian
+ be2me ... BigEndian to MachineEndian
+ le2me ... LittleEndian to MachineEndian
 */
 
 #ifdef WORDS_BIGENDIAN
