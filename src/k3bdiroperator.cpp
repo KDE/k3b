97a5e1f0423a9463a5c8a2747336c1f4d9d890ca
Nicolas Fella <nicolas.fella@gmx.de>
2023-08-20 22:25:04 +0200

    Adapt to KBookmarks API change
    setEditorOptions is no longer necessary in KF6

--- src/k3bdiroperator.cpp
+++ src/k3bdiroperator.cpp
@@ -41,10 +41,10 @@ K3b::DirOperator::DirOperator(const QUrl& url, QWidget* parent )
 
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     KBookmarkManager* bmMan = KBookmarkManager::managerForFile( bookmarksFile, "k3b" );
+    bmMan->setEditorOptions( i18n("K3b Bookmarks"), false );
 #else
     KBookmarkManager* bmMan = KBookmarkManager::managerForFile( bookmarksFile );
 #endif
-    bmMan->setEditorOptions( i18n("K3b Bookmarks"), false );
     bmMan->setUpdate( true );
 
     m_bmPopup = new KActionMenu( QIcon::fromTheme("bookmarks"),i18n("Bookmarks"), this);
